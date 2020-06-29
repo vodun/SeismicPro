60b777df5191a186cc909c68c5f0a142636669d7
a-arefina <7520522+a-arefina@users.noreply.github.com>
2020-06-29 11:46:03 +0300

    PR comments

--- seismicpro/src/seismic_batch.py
+++ seismicpro/src/seismic_batch.py
@@ -1469,47 +1469,6 @@ class SeismicBatch(Batch):
         return self
 
 
-    @staticmethod
-    def __crop_from_image(image, coords, shape, pad_zeros):
-        """ Perform crops from the image.
-        Number of crops is defined by the number of elements in `coords` parameter.
-
-        Parameters
-        ----------
-        image : np.array
-            Image to crop from.
-        coords: list of tuples
-            The list of top-left (x,y) coordinates for each crop.
-        shape: tuple of ints
-            Crop shape.
-        pad_zeros: bool
-            Wether to zero-pad incomplete crops
-
-        Returns
-        -------
-        res: np.array, dtype='O'
-            Array with crops.
-        """
-        res = np.empty((len(coords), ), dtype='O')
-        for i, (x, y) in enumerate(coords):
-            if pad_zeros:
-                arr = np.zeros(shape)
-
-                x1 = min(x + shape[0], image.shape[0])
-                y1 = min(y + shape[1], image.shape[1])
-
-                arr[:x1-x, :y1-y] = image[x:x1, y:y1]
-                res[i] = arr
-
-            else:
-                if (x + shape[0] > image.shape[0]) or (y + shape[1] > image.shape[1]):
-                    raise ValueError('Coordinates', (x, y), 'exceed feasible region of seismogram with shape',
-                                     image.shape, ', with crop shape', shape, 'but pad_zeros is False')
-                res[i] = image[x:x+shape[0], y:y+shape[1]]
-
-        return res
-
-
     @inbatch_parallel(init='indices')
     def __crop(self, index, src, coords, shape, pad_zeros, dst=None):
         """ Generate crops from an array with seismic data
@@ -1526,7 +1485,24 @@ class SeismicBatch(Batch):
         else:
             xy = np.array(coords)
 
-        getattr(self, dst)[pos] = self.__crop_from_image(arr, xy, shape, pad_zeros)
+        res = np.empty((len(xy), ), dtype='O')
+        for i, (x, y) in enumerate(xy):
+            if pad_zeros:
+                crop = np.zeros(shape)
+
+                x1 = min(x + shape[0], arr.shape[0])
+                y1 = min(y + shape[1], arr.shape[1])
+
+                crop[:x1-x, :y1-y] = arr[x:x1, y:y1]
+                res[i] = crop
+
+            else:
+                if (x + shape[0] > arr.shape[0]) or (y + shape[1] > arr.shape[1]):
+                    raise ValueError('Coordinates', (x, y), 'exceed feasible region of seismogram with shape',
+                                     arr.shape, ', with crop shape', shape, 'but pad_zeros is False')
+                res[i] = arr[x:x+shape[0], y:y+shape[1]]
+
+        getattr(self, dst)[pos] = res
 
         self.meta[dst]['crop_coords'][index] = xy
 
