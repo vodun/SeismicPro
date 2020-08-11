f90d20ed3bfa0f2e55f5c2b1b278e5f71082b703
anton-br <ziliboba753@gmail.com>
2020-08-11 15:47:42 +0300

    add metrics plot function

--- seismicpro/src/seismic_metrics.py
+++ seismicpro/src/seismic_metrics.py
@@ -1,29 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from numba import njit, prange
 
+from .utils import construct_metrics_map
+from .plot_utils import plot_metrics_map
 from ..batchflow.models.metrics import Metrics
 
 METRICS_ALIASES = {
     'minmax': 'minmax_map',
     'std': 'std_map'
 }
+
+def _aggr_func(metrics):
+    return np.nanmean(metrics)
+
 class SeismicMetrics(Metrics):
-    """"""
+    """seismic metrics class"""
     def __init__(self, index, diff_semblance, *args, map_type='receivers', _calc=True, **kwargs):
         super().__init__()
         self.diff = diff_semblance
         self.coords = None
-        self.coords = np.array([*index])
-        # if map_type == 'receivers':
-        #     self.coords = np.array(index.get_df()[["SourceX", "SourceY"]])[0]
-        # elif map_type == 'shots':
+        if map_type == 'receivers':
+            self.coords = np.array(index.get_df()[["SourceX", "SourceY"]])[0]
+        elif map_type == 'shots':
             # TODO find out where are shots stored.
-            # pass
-
+            pass
         self.resulted_list = [[*self.coords, self.diff]]
+        self._agg_fn_dict = {'mean': np.nanmean,
+                             'max': np.nanmax,
+                             'min': np.nanmin}
 
     def append(self, metrics):
+        """append"""
         self.resulted_list.append(metrics.resulted_list[0])
 
     def __getattr__(self, name):
@@ -33,52 +39,30 @@ class SeismicMetrics(Metrics):
         return object.__getattribute__(self, name)
 
     def _split_result(self):
-        coords_x = []
-        coords_y = []
-        diff = []
-        for x, y, d in self.resulted_list:
-            coords_x.append(x)
-            coords_y.append(y)
-            diff.append(d)
-        # resulted_list = np.array(self.resulted_list)
-        return np.array(coords_x), np.array(coords_y), np.array(diff)
+        """split_result"""
+        coords_x, coords_y, diff = np.array(self.resulted_list).T
+        diff = np.array(list(diff), dtype=np.float32)
+        return np.array(coords_x, dtype=np.float32), np.array(coords_y, dtype=np.float32), diff
 
-    # @njit(parallel=True)
-    def calc_metric(self, rps_x, rps_y, rps_val, bin_size):
-        x_min = rps_x.min()
-        x_max = rps_x.max()
-        x = np.arange(x_min, x_max, bin_size)
-
-        y_min = rps_y.min()
-        y_max = rps_y.max()
-        y = np.arange(y_min, y_max, bin_size)
-
-        metric_map = np.full((len(y), len(x)), np.nan)
-        print(metric_map)
-        for i in prange(len(x)):
-            for j in prange(len(y)):
-                mask = ((rps_x - x[i] > 0) & (rps_x - x[i] < bin_size) &
-                        (rps_y - y[j] > 0) & (rps_y - y[j] < bin_size))
-                if mask.sum() > 0:
-                    metric_map[j, i] = rps_val[mask].mean()
-        return metric_map
-
-    def minmax_map(self, bin_size=500):
+    def minmax_map(self, bin_size=500, max_value=None, title=None, figsize=None, save_dir=None):
+        """map"""
         coords_x, coords_y, diff = self._split_result()
-
         metrics = np.max(np.max(diff, axis=2) - np.min(diff, axis=2), axis=1)
 
-        print(type(coords_x), type(coords_y), type(metrics))
-        metric_map = self.calc_metric(rps_x=coords_x, rps_y=coords_y, rps_val=metrics, bin_size=bin_size)
-        print(metric_map)
-        plt.imshow(metric_map, origin='lower')#, extent=[x_min, x_max, y_min, y_max])
-        return metric_map
+        metrics_map = construct_metrics_map(coords_x=coords_x, coords_y=coords_y, metrics=metrics, bin_size=bin_size)
+        extent_coords = [coords_x.min(), coords_x.max(), coords_y.min(), coords_y.max()]
+        plot_metrics_map(metrics_map=metrics_map, max_value=max_value, extent_coords=extent_coords,
+                         title=title, figsize=figsize, save_dir=save_dir)
 
-    def std_map(self, bin_size=500):
+        return metrics_map
+
+    def std_map(self, bin_size=500, max_value=None, title=None, figsize=None, save_dir=None):
+        """map"""
         coords_x, coords_y, diff = self._split_result()
-
         metrics = np.max(np.std(diff, axis=2), axis=1)
 
-        metric_map = self.calc_metric(coords_x, coords_y, metrics, bin_size)
-        plt.imshow(metric_map, origin='lower')#, extent=[x_min, x_max, y_min, y_max])
-
+        metrics_map = construct_metrics_map(coords_x=coords_x, coords_y=coords_y, metrics=metrics, bin_size=bin_size)
+        extent_coords = [coords_x.min(), coords_x.max(), coords_y.min(), coords_y.max()]
+        plot_metrics_map(metrics_map=metrics_map, max_value=max_value, extent_coords=extent_coords,
+                         title=title, figsize=figsize, save_dir=save_dir)
+        return metrics_map
