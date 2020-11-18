5ac9f5ec4308f7cdd5c37cdca9da0d05a27ddeea
anton-br <ziliboba753@gmail.com>
2020-11-18 11:28:41 +0300

    update docs

--- seismicpro/src/seismic_metrics.py
+++ seismicpro/src/seismic_metrics.py
@@ -18,31 +18,38 @@ class MetricsMap(Metrics):
         Array with coordinates for X and Y axes.
     kwargs : dict
         All given kwargs are considered quality values. The key value is any name,
-        and the value must be either number or a 1-dimensional array.
+        and the value of the metric can be represented by a number, a one-dimensional vector,
+        or an array of one-dimensional arrays.
+        A metric can only be a number if one coordinate was passed, then the metric value
+        will correspond to that coordinate.
+        If the metric is a one-dimensional vector, each value from the array will correspond
+        to its own pair of coordinates.
+        If the metric is an array of arrays, each coordinate will have an array suit to it.
 
     Attributes
     ----------
     metrics_names : array-like
-        Names of given metrics.
+        Names of given metrics and coords.
     coords : array-like
         Array with shape (N, 2) that contains the X and Y coordinates.
         Where N is a number of given coordinates.
     agg_func : str or callable
         See :meth:`.construct_map`
     call_name : str
-        Contains the name of used aggregation function.
+        Contains the name of the used aggregation function.
 
     Raises
     ------
     ValueError : If kwargs are empty.
-    ValueError : If given coordinates has more then two values for one metrics value.
-    TypeError : If given coordinates is not array-like.
+    ValueError : If given coordinates have more than two values for one metrics value.
+    TypeError : If given coordinates are not array-like.
     TypeError : If the metric is not a number or an array.
     ValueError : If the length of the metric array does not match the length of the array with coordinates.
 
     Note
     ----
     1. All keys from kwargs become class attributes.
+    2. The length of the metric array and the coordinate array must match.
     """
 
     def __init__(self, coords, **kwargs):
@@ -96,32 +103,34 @@ class MetricsMap(Metrics):
 
     def construct_map(self, metrics_name, bin_size=500, agg_func='mean',
                       agg_func_kwargs=None, plot=True, **plot_kwargs):
-        """ All optained coordinates, are splitted into bins of the specified `bin_size`. Resulted map
-        is an array in which every value reflects the metric's value in one current bin. If there are no values
-        are included in the bin, it's value is np.nan. Otherwise, the value of this bin is calculated based
-        on the aggregation function `agg_func`.
+        """ All optained coordinates are split into bins of the specified `bin_size`. The resulted map
+        is an array in which every value reflects the metric's value in one current bin. If there are
+        no values are included in the bin, it values is np.nan. Otherwise, the value of this bin is
+        calculated based on the aggregation function `agg_func`.
 
-        Each value in resulted map represent aggregated value of metrics for coordinates belongs to current bin.
+        Each value in the resulted map represents the aggregated value of metrics for coordinates
+        belongs to the current bin.
 
         Parameters
         ----------
         metrics_name : str
-            The name of metrics to draw.
-        bin_size : int or array-like with length 2, optional, default 500
-            The size of bin by X and Y axes. Based on the received coordinates, the entire map
-            will be divided into bins with size `bin_size`.
-            If int, the bin size will be same for X and Y dimensions.
+            The name of metric to draw.
+        bin_size : int, float or array-like with length 2, optional, default 500
+            The size of the bin by X and Y axes. Based on the received coordinates, the entire map
+            will be divided into bins with the size `bin_size`.
+            If int, the bin size will be the same for X and Y dimensions.
         agg_func : str or callable, optional, default 'mean'
             Function to aggregate metrics values in one bin.
-        If str, the function from :class:`.NumbaNumpy` will be used for aggregation.
-        If callable, it will be used for aggregation as this. The function used must be wrapped in the
-        njit decorator. The first argument is data for aggregation within the bin, the other arguments
-        can take any numeric values and must be passed using the `agg_func_kwargs`.
+            If str, the function from :class:`.NumbaNumpy` will be used for aggregation.
+            If callable, it will be used for aggregation as this. The function used must
+            be wrapped in the `njit` decorator. The first argument is data for aggregation
+            within the bin, the other arguments can take any numeric values and must be
+            passed using the `agg_func_kwargs`.
         agg_func_kwargs : dict, optional
             Kwargs that will be applied to agg_func before evaluating.
         plot : bool, optional, default True
             If True, metrics will be plotted.
-            Otherwise the map will not be drawn.
+            Otherwise, the map will be returned without drawing.
         **plot_kwargs : dict
             Kwargs that are passed directly to plotter, see :func:`.plot_utils.plot_metrics_map`.
 
@@ -130,10 +139,14 @@ class MetricsMap(Metrics):
             : two-dimensional np.ndarray
             A matrix, where each value corresponds to the aggregated value
             of all metrics included in a specific bin.
+
+        Raises
+        ------
+        ValueError : If agg_func is not str or callable.
         """
         metrics = getattr(self, metrics_name)
 
-        # If metrics has an array for one coordinate, we repeat the coordinate value
+        # If metric has an array for one coordinate, we repeat the coordinate value
         # and expand the metric values into a one-dimensional array.
         if isinstance(metrics[0], (list, tuple, set, np.ndarray)):
             len_of_copy = [len(metrics_array) for metrics_array in metrics]
@@ -170,7 +183,7 @@ class MetricsMap(Metrics):
 
         if plot:
             extent = [coords_x.min(), coords_x.max(), coords_y.min(), coords_y.max()]
-            # Avoid the situation when we have only one coordinate for x or y dimension.
+            # Avoid the situation when we have only one unique coordinate for x or y dimension.
             # Because in this case our maximum and minimum values for this axis will be
             # the same, and imshow will draw an empty image.
             extent[1] += 1 if extent[0] - extent[1] == 0 else 0
@@ -195,6 +208,11 @@ class MetricsMap(Metrics):
             The size of bin by X and Y axes.
         arrg_func : numba callable
             Function to aggregate metrics values in one bin.
+
+        Returns
+        -------
+            : two-dimensional array
+            The resulting map with aggregated metric values by bins.
         """
         bin_size_x, bin_size_y = bin_size
         range_x = np.arange(coords_x.min(), coords_x.max() + 1, bin_size_x)
