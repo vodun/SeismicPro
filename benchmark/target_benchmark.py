768c31d290b047688a8f7e8409cf6b96e1a9344b
anton-br <ziliboba753@gmail.com>
2021-03-09 14:23:51 +0300

    update benchmark

--- benchmark/target_benchmark.py
+++ benchmark/target_benchmark.py
@@ -19,37 +19,36 @@ def benchmark_method(method, dataset, targets, batch_sizes, n_iters=10, root_ppl
 
     root_pipeline = Pipeline()
     if root_ppl is not None:
-        if isinstance(root_ppl, str):
+        if root_ppl in ['load', 'sort']:
             root_pipeline += Pipeline().load(src='raw', fmt='segy')
             if root_ppl == 'sort':
                 root_pipeline += Pipeline().sort(src='raw', dst='raw', by='offset')
-            elif root_ppl != 'load':
-                raise ValueError('Wrong root_ppl name.')
-        elif not isinstance(root_ppl, Pipeline):
-            raise ValueError('Wrong root_ppl type.')
-        else:
+        elif isinstance(root_ppl, Pipeline):
             root_pipeline += root_ppl
+        else:
+            raise ValueError('Wrong root_ppl type.')
 
-    times_dict = dict()
-    notifiers_dict = dict()
+
+    method_results = defaultdict(list)
+    notifiers_results = defaultdict(list)
     for target in tqdm(targets):
         for batch_size in tqdm(batch_sizes):
             step_times, notifiers = _benchmark_step(method=method, dataset=dataset, root_pipeline=root_pipeline,
                                                     target=target, batch_size=batch_size, n_iters=n_iters,
                                                     notifier_monitors=notifier_monitors, **method_kwargs)
-            step_name = f'{target}_{batch_size}'
-            times_dict[step_name] = step_times
+            method_results[target].append(step_times)
 
             for notifier in notifiers:
-                if notifier['name'] not in notifiers_dict.keys():
-                    notifiers_dict[notifier['name']] = dict(ylabel=notifier['source'].UNIT)
-                notifiers_dict[notifier['name']][f'{step_name}'] = notifier['data']
+                if notifier['name'] not in notifiers_results.keys():
+                    notifiers_results[notifier['name']] = defaultdict(list, ylabel=notifier['source'].UNIT)
+                notifiers_results[notifier['name']][target].append(notifier['data'])
 
-    benchmark_plotter(res_dict=times_dict, title=method.__name__, ylabel='Time (s)', figsize=figsize)
-    for monitor_name, results in notifiers_dict.items():
+    benchmark_plotter(res_dict=method_results, title=method.__name__, ylabel='Time (s)',
+                      xticks=batch_sizes, figsize=figsize)
+    for monitor_name, results in notifiers_results.items():
         ylabel = results.pop('ylabel')
-        benchmark_plotter(res_dict=results, title=monitor_name, ylabel=ylabel, figsize=figsize)
-    return times_dict, notifiers_dict
+        benchmark_plotter(res_dict=results, title=monitor_name, ylabel=ylabel, xticks=batch_sizes, figsize=figsize)
+    return method_results, notifiers_results
 
 def _benchmark_step(method, dataset, root_pipeline, target, batch_size, n_iters, notifier_monitors, **kwargs):
     kwargs.update(target=target)
@@ -58,6 +57,7 @@ def _benchmark_step(method, dataset, root_pipeline, target, batch_size, n_iters,
     main_pipeline.run(batch_size, n_iters=n_iters, shuffle=42, drop_last=False, bar=False, profile=True,
                       notifier=Notifier(monitors=notifier_monitors))
 
+    # We assume that our method is the last action in pipeline.
     action_name = method.__name__ + f' #{main_pipeline.num_actions-1}'
     time_df = main_pipeline.show_profile_info(per_iter=True, detailed=False)
     time_vals = time_df['total_time'].loc[:, action_name].values
@@ -66,23 +66,16 @@ def _benchmark_step(method, dataset, root_pipeline, target, batch_size, n_iters,
     return time_vals, notifier
 
 
-def benchmark_plotter(res_dict, title=None, ylabel=None, figsize=None):
+def benchmark_plotter(res_dict, title=None, ylabel=None, xticks=None, figsize=None):
     plt.figure(figsize=figsize)
-    target_dict = defaultdict(list)
-    iters = []
 
-    for bench_name, res in res_dict.items():
-        target_name, batch_size = bench_name.split('_')
-        target_dict[target_name].append(res)
-        iters.append(int(batch_size))
-    iters = np.unique(iters)
-
-    for target, times in target_dict.items():
+    for target, times in res_dict.items():
         x_axis = np.concatenate([[ix] * len(time) for ix, time in enumerate(times)])
         sns.lineplot(x=x_axis, y=np.concatenate(times), label=target, ci='sd')
 
-    plt.xticks(ticks=np.arange(len(iters)), labels=iters)
     plt.xlabel('Batch size')
+    if xticks:
+        plt.xticks(ticks=np.arange(len(xticks)), labels=xticks)
     if title:
         plt.title(title)
     if ylabel:
