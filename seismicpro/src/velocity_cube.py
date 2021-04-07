b58cf786f7c47305fffea9a69c1855c703de575f
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-04-07 15:12:45 +0300

    Fix nd interpolator for degenerate case

--- seismicpro/src/velocity_cube.py
+++ seismicpro/src/velocity_cube.py
@@ -1,6 +1,43 @@
 import numpy as np
+import cv2
 from scipy.interpolate import interp1d, LinearNDInterpolator
 from sklearn.neighbors import NearestNeighbors
+
+
+class VelocityInterpolator:
+    def __init__(self, laws):
+        self.laws = laws
+        self.coords = np.stack(list(laws.keys()))
+        self.coords_hull = cv2.convexHull(self.coords, returnPoints=True)
+
+        self.nearest_interpolator = NearestNeighbors(n_neighbors=1)
+        self.nearest_interpolator.fit(self.coords)
+
+        min_i, min_x = np.min(self.coords, axis=0) - 1
+        max_i, max_x = np.max(self.coords, axis=0) + 1
+        fake_laws = [self._interpolate_nearest(i, x)
+                     for i, x in [(min_i, min_x), (min_i, max_x), (max_i, min_x), (max_i, max_x)]]
+        law_data = np.concatenate([np.concatenate([[(law.inline, law.crossline)] * len(law.model), law.model], axis=1)
+                                   for law in list(laws.values()) + fake_laws])
+        self.linear_interpolator = LinearNDInterpolator(law_data[:, :-1], law_data[:, -1])
+
+    def is_in_hull(self, inline, crossline):
+        return cv2.pointPolygonTest(self.coords_hull, (inline, crossline), measureDist=True) >= 0
+
+    def _interpolate_linear(self, inline, crossline):
+        velocity_interpolator = lambda times: self.linear_interpolator(inline, crossline, times)
+        return VelocityLaw(inline, crossline, interpolator=velocity_interpolator)
+
+    def _interpolate_nearest(self, inline, crossline):
+        index = self.nearest_interpolator.kneighbors([(inline, crossline),], return_distance=False).item()
+        nearest_inline, nearest_crossline = self.coords[index].tolist()
+        nearest_law = self.laws[(nearest_inline, nearest_crossline)]
+        return VelocityLaw(inline, crossline, nearest_law.model, nearest_law.interpolator)
+
+    def __call__(self, inline, crossline):
+        if self.is_in_hull(inline, crossline):
+            return self._interpolate_linear(inline, crossline)
+        return self._interpolate_nearest(inline, crossline)
 
 
 class VelocityLaw:
@@ -20,53 +57,21 @@ class VelocityLaw:
         return self.interpolator(times)
 
 
-class NearestInterpolator:
-    def __init__(self, laws):
-        self.laws = laws
-        self.coords = np.stack(list(laws.keys()))
-        self.nn = NearestNeighbors(n_neighbors=1)
-        self.nn.fit(self.coords)
-
-    def __call__(self, inline, crossline):
-        index = self.nn.kneighbors([(inline, crossline),], return_distance=False).item()
-        nn_inline, nn_crossline = self.coords[index].tolist()
-        nn_interpolator = self.laws[(nn_inline, nn_crossline)]
-        return VelocityLaw(nn_inline, nn_crossline, interpolator=nn_interpolator)
-
-
-class LinearInterpolator:
-    def __init__(self, laws):
-        law_data = np.concatenate([np.concatenate([[(law.inline, law.crossline)] * len(law.model), law.model], axis=1)
-                                   for law in laws.values()])
-        self.interpolator = LinearNDInterpolator(law_data[:, :-1], law_data[:, -1])
-
-    def is_in_hull(self, inline, crossline):
-        res = self.interpolator(inline, crossline, 0).item()
-        return not np.isnan(res)
-
-    def __call__(self, inline, crossline):
-        return VelocityLaw(inline, crossline, interpolator=lambda times: self.interpolator(inline, crossline, times))
-
-
 class VelocityCube:
     def __init__(self):
         self.laws = {}
-        self.linear_interpolator = None
-        self.nearest_interpolator = None
+        self.velocity_interpolator = None
 
     def update(self, law):
         self.laws[(law.inline, law.crossline)] = law
         return self
 
-    def create_interpolator(self):
+    def create_velocity_interpolator(self):
         # TODO: interpolate times to a given range
-        self.linear_interpolator = LinearInterpolator(self.laws)
-        self.nearest_interpolator = NearestInterpolator(self.laws)
+        self.velocity_interpolator = VelocityInterpolator(self.laws)
         return self
 
     def get_law(self, inline, crossline):
-        if self.linear_interpolator is None:
-            raise ValueError("Interpolator must be created first")
-        if self.linear_interpolator.is_in_hull(inline, crossline):
-            return self.linear_interpolator(inline, crossline)
-        return self.nearest_interpolator(inline, crossline)
+        if self.velocity_interpolator is None:
+            raise ValueError("Velocity interpolator must be created first")
+        return self.velocity_interpolator(inline, crossline)
