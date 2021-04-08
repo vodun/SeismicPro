a8611bcd1ab30200502247a01e4353e9525c721c
anton-br <ziliboba753@gmail.com>
2021-04-08 15:00:27 +0300

    add assign to named_expr

--- seismicpro/src/named_expr.py
+++ seismicpro/src/named_expr.py
@@ -2,8 +2,11 @@ import sys
 
 import numpy as np
 
+from .utils import to_list
+
 from ..batchflow import NamedExpression
 from ..batchflow.batchflow.named_expr import _DummyBatch
+
 
 
 class Component:
@@ -17,17 +20,21 @@ class Component:
         if name == 'component':
             self.__dict__[name] = value
         else:
+            if len(self.component) != len(value):
+                raise ValueError("Given `value`'s length must be equal to batch size.")
             for item, val in zip(self.component, value):
                 setattr(item, name, val)
 
     def __getitem__(self, key):
-        # note, the __getitem__ method must be overridden in the component class.
+        # note, the __getitem__ method must be overridden in the component's class.
         return np.array([val[key] for val in self.component])
 
     def __setitem__(self, key, value):
-        key = np.array(key).ravel().tolist()
+        key = to_list(key)
+        if len(self.component) != len(value):
+                raise ValueError("Given `value`'s length must be equal to batch size.")
         for item, val in zip(self.component, value):
-            # note, the __setitem__ method must be overridden in the component class.
+            # note, the __setitem__ method must be overridden in the component's class.
             item[key] = val
 
 
@@ -43,3 +50,10 @@ class SU(NamedExpression):
             # TODO: rewrite messsage
             raise ValueError("Batch expressions are not allowed in static model")
         return Component(getattr(batch, name))
+
+    def assign(self, value, batch=None, pipeline=None, model=None):
+        if self.params:
+            batch, pipeline, model = self.params
+        name = super()._get_name(batch=batch, pipeline=pipeline, model=model)
+        if name is not None:
+            setattr(batch, name, value)
