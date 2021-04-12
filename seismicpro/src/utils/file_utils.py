1539afeefd45f87db097a12f1a0187ddd6565d85
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-04-12 14:46:54 +0300

    Rewrite aggregate_segys

--- seismicpro/src/utils/file_utils.py
+++ seismicpro/src/utils/file_utils.py
@@ -3,62 +3,45 @@ import glob
 
 import segyio
 import numpy as np
-from tqdm import tqdm
+from tqdm.auto import tqdm
+
+from .general_utils import to_list
 
 
-FILE_EXTS = ['.sgy', '.segy']
+def aggregate_segys(in_paths, out_path, recursive=False, mmap=True, keep_exts=("sgy", "segy"), bar=True):
+    in_paths = sum([glob.glob(path, recursive=recursive) for path in to_list(in_paths)], [])
+    if keep_exts is not None:
+        in_paths = [path for path in in_paths if os.path.splitext(path)[1][1:] in keep_exts]
+    if not in_paths:
+        raise ValueError("No files match given pattern")
 
+    # Check whether all files have the same trace length and sample rate
+    source_handlers = [segyio.open(path, ignore_geometry=True) for path in in_paths]
+    samples = source_handlers[0].samples
+    if not all(np.array_equal(samples, handler.samples) for handler in source_handlers[1:]):
+        raise ValueError("Source files contain inconsistent samples")
 
-def aggregate_segys(path, out_path, recursive=False, bar=True):
-    """ Path is either path to file or just to needed dir (with *.sgy or without).
-    recursive goes directly into glob.glob. Is allows to add files from a nested structure via using `**`
-    in path.
-    """
-    path = (path, ) if isinstance(path, str) else path
+    if mmap:
+        _ = [source_handler.mmap() for source_handler in source_handlers]
 
-    files = []
-    for p in path:
-        # If given p is a dir name, we gonna find all files with extensions from FILE_EXTS.
-        if not os.path.splitext(p)[1]:
-            pattern_list = [os.path.join(p,'*'+ext) for ext in FILE_EXTS]
-            concat_path = np.concatenate([glob.glob(pattern, recursive=recursive) for pattern in pattern_list])
-            files.extend(concat_path)
-        else:
-            files.extend(glob.glob(p, recursive=recursive))
-
-    # Check wether the files have same trace length and sample ratio and calculate number of traces
-    # to allocate required buffer size for creating a new segy.
-    tracecount = 0
-    samples = None
-    handlers_aggr = []
-    for fname in files:
-        segy_handler = segyio.open(fname, strict=False)
-        segy_handler.mmap()
-        if samples is None:
-            samples = segy_handler.samples
-            ext_headers = segy_handler.ext_headers
-            segy_format = segy_handler.format
-
-        if np.any(samples != segy_handler.samples):
-            raise ValueError("Inconsistent samples in files!" +
-                                f"Samples is {segy_handler.samples} in {fname}, previous value was {samples}")
-        tracecount += segy_handler.tracecount
-        handlers_aggr.append(segy_handler)
-
-    # Create segyio spec for new file. We choose only specs that relate to unstructured data.
+    # Create segyio spec for the new file
     spec = segyio.spec()
     spec.samples = samples
-    spec.ext_headers = ext_headers
-    spec.format = segy_format
-    spec.tracecount = tracecount
+    spec.ext_headers = source_handlers[0].ext_headers
+    spec.format = source_handlers[0].format
+    spec.tracecount = sum(handler.tracecount for handler in source_handlers)
 
-    # Write traces and headers from `files` into new file.
-    with segyio.create(out_path, spec) as to_handler:
-        tr_pos = 0
-        handlers_iterable = tqdm(handlers_aggr) if bar else handlers_aggr
-        for from_handler in handlers_iterable:
-            to_handler.trace[tr_pos: tr_pos + from_handler.tracecount] = from_handler.trace
-            to_handler.header[tr_pos: tr_pos + from_handler.tracecount] = from_handler.header
-            tr_pos += from_handler.tracecount
-        for i in range(tracecount):
-            to_handler.header[i].update({segyio.TraceField.TRACE_SEQUENCE_FILE: i + 1})
+    # Write traces and their headers from source files into the new one
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    with segyio.create(out_path, spec) as out_handler:
+        trace_pos = 0
+        for source_handler in tqdm(source_handlers, disable=not bar):
+            out_handler.trace[trace_pos : trace_pos + source_handler.tracecount] = source_handler.trace
+            out_handler.header[trace_pos : trace_pos + source_handler.tracecount] = source_handler.header
+            trace_pos += source_handler.tracecount
+        for i in range(out_handler.tracecount):
+            out_handler.header[i].update({segyio.TraceField.TRACE_SEQUENCE_FILE: i + 1})
+
+    # Close source segy file handlers
+    for source_handler in source_handlers:
+        source_handler.close()
