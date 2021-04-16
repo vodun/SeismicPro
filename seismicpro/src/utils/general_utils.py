41a7b47cd1c9f1f4cd3d755dc6f970b3ef1303de
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-04-16 16:03:14 +0300

    Avoid range(len())

--- seismicpro/src/utils/general_utils.py
+++ seismicpro/src/utils/general_utils.py
@@ -14,11 +14,11 @@ def to_list(obj):
 def calculate_stats(trace):
     trace_min, trace_max = np.inf, -np.inf
     trace_sum, trace_sq_sum = 0, 0
-    for i in range(len(trace)):
-        trace_min = min(trace[i], trace_min)
-        trace_max = max(trace[i], trace_max)
-        trace_sum += trace[i]
-        trace_sq_sum += trace[i]**2
+    for sample in trace:
+        trace_min = min(sample, trace_min)
+        trace_max = max(sample, trace_max)
+        trace_sum += sample
+        trace_sq_sum += sample**2
     return trace_min, trace_max, trace_sum, trace_sq_sum
 
 
@@ -28,24 +28,22 @@ def create_supergather_index(lines, size):
     shifts_i = np.arange(size[0]) - size[0] // 2
     shifts_x = np.arange(size[1]) - size[1] // 2
     supergather_lines = np.empty((len(lines) * area_size, 4), dtype=lines.dtype)
-    for ix in range(len(lines)):
-        i, x = lines[ix]
-        for ix_i in range(size[0]):
-            for ix_x in range(size[1]):
-                row = np.array([i, x, i + shifts_i[ix_i], x + shifts_x[ix_x]])
+    for ix, (i, x) in enumerate(lines):
+        for ix_i, shift_i in enumerate(shifts_i):
+            for ix_x, shift_x in enumerate(shifts_x):
+                row = np.array([i, x, i + shift_i, x + shift_x])
                 supergather_lines[ix * area_size + ix_i * size[1] + ix_x] = row
     return supergather_lines
 
 
 @njit(parallel=True)
 def convert_mask_to_pick(mask, threshold):
-    mask = mask > threshold
-    picking_list = np.empty(len(mask), dtype=np.int32)
+    picking_array = np.empty(len(mask), dtype=np.int32)
     for i in prange(len(mask)):
         trace = mask[i]
         max_len, curr_len, picking_ix = 0, 0, 0
-        for j in range(len(trace)):
-            if trace[j] == 1:
+        for j, sample in enumerate(trace):
+            if sample >= threshold:
                 curr_len += 1
             else:
                 if curr_len > max_len:
@@ -54,5 +52,5 @@ def convert_mask_to_pick(mask, threshold):
                 curr_len = 0
         if curr_len > max_len:
             picking_ix = len(trace)
-        picking_list[i] = picking_ix - curr_len
-    return picking_list
+        picking_array[i] = picking_ix - curr_len
+    return picking_array
