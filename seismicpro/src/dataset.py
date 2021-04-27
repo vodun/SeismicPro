d89389220579186e27993f238aa5d1cc786842cf
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-04-27 15:22:11 +0300

    Clarify raise in SeismicDataset, make its __init__ arguments keyword-only

--- seismicpro/src/dataset.py
+++ seismicpro/src/dataset.py
@@ -1,10 +1,10 @@
-from ..batchflow import Dataset
 from .batch import SeismicBatch
 from .index import SeismicIndex
+from ..batchflow import Dataset
 
 
 class SeismicDataset(Dataset):
-    def __init__(self, index=None, batch_class=SeismicBatch, preloaded=None, copy=True, **kwargs):
+    def __init__(self, *, index=None, batch_class=SeismicBatch, preloaded=None, copy=True, **kwargs):
         if index is None:
             index = SeismicIndex(**kwargs)
         super().__init__(index, batch_class=batch_class, preloaded=preloaded, copy=copy, **kwargs)
@@ -12,6 +12,6 @@ class SeismicDataset(Dataset):
     def create_subset(self, index):
         if isinstance(index, SeismicIndex) and isinstance(self.index, SeismicIndex):
             if not index.indices.isin(self.indices).all():
-                raise IndexError
+                raise IndexError("Some indices from given index are not present in current SeismicIndex")
             return type(self).from_dataset(self, self.index.create_subset(index))
         return super().create_subset(index)
