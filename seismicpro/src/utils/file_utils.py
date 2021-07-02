e211cfe13a46df2fb11cb338eeb35792aa717cf2
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-07-02 16:26:19 +0300

    Clarify docs in read_vfunc and read_single_vfunc

--- seismicpro/src/utils/file_utils.py
+++ seismicpro/src/utils/file_utils.py
@@ -75,7 +75,11 @@ def aggregate_segys(in_paths, out_path, recursive=False, mmap=True, keep_exts=("
 
 
 def read_vfunc(path):
-    """Read a file with vertical functions.
+    """Read a file with vertical functions in Paradigm Echos VFUNC format.
+
+    The file may have one or more records with the following structure:
+    VFUNC [inline] [crossline]
+    [x1] [y1] [x2] [y2] ... [xn] [yn]
 
     Parameters
     ----------
@@ -107,7 +111,11 @@ def read_vfunc(path):
 
 
 def read_single_vfunc(path):
-    """Read a single vertical function from a file.
+    """Read a single vertical function from a file in Paradigm Echos VFUNC format.
+
+    The file must have exactly one record with the following structure:
+    VFUNC [inline] [crossline]
+    [x1] [y1] [x2] [y2] ... [xn] [yn]
 
     Parameters
     ----------
@@ -130,6 +138,7 @@ def read_single_vfunc(path):
     if len(file_data) != 1:
         raise ValueError(f"Input file must contain a single vfunc, but {len(file_data)} were found in {path}")
     return file_data[0]
+
 
 def make_prestack_segy(path_segy, sources_size=500, activation_dist=500, bin_size=50, samples=1500, trace_gen=None,
                        dist_source_lines=300, dist_sources=50, dist_reciever_lines=100, dist_recievers=25, **kwargs):
