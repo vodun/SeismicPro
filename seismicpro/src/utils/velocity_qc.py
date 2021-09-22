14e9e6d603ff3bb583f4b051c067440bbbafa4bf
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-09-22 16:53:02 +0300

    Rename velocity QC functions

--- seismicpro/src/utils/velocity_qc.py
+++ seismicpro/src/utils/velocity_qc.py
@@ -2,6 +2,9 @@ import numpy as np
 from numba import njit, prange
 
 
+VELOCITY_QC_METRICS = ["is_decreasing", "max_standard_deviation", "max_relative_variation"]
+
+
 @njit(nogil=True)
 def is_decreasing(window):
     for i in range(window.shape[1]):
@@ -11,7 +14,7 @@ def is_decreasing(window):
 
 
 @njit(nogil=True)
-def max_std(window):
+def max_standard_deviation(window):
     max_std = 0
     for i in range(window.shape[1]):
         current_std = window[:, i].std()
@@ -20,16 +23,9 @@ def max_std(window):
 
 
 @njit(nogil=True)
-def max_mean_variation(window):
+def max_relative_variation(window):
     max_mean_var = 0
     for i in range(window.shape[1]):
-        current_mean_var = (np.mean(window[:, i]) - window[0, i]) / window[0, i]
+        current_mean_var = abs(np.mean(window[1:, i]) - window[0, i]) / window[0, i]
         max_mean_var = max(max_mean_var, current_mean_var)
     return max_mean_var
-
-
-VELOCITY_QC_METRICS = {
-    "is_decreasing": is_decreasing,
-    "max_std": max_std,
-    "max_mean_variation": max_mean_variation,
-}
