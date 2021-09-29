c03f4b40c4cd293ddf20ca9beccd0ab5011e837e
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-09-29 13:08:02 +0300

    Add interpolation docs

--- seismicpro/src/utils/interpolation.py
+++ seismicpro/src/utils/interpolation.py
@@ -1,9 +1,13 @@
+"""Implements a class for linear 1d interpolation and extrapolation"""
+
 import numpy as np
-from numba import njit, prange
+from numba import njit
 
 
 @njit(nogil=True)
 def interpolate(x_new, x, y, left_slope, right_slope):
+    """Return a 1d piecewise linear interpolant to a function defined by pairs of data points `(x, y)`, evaluated at
+    `x_new`. Function values at points outside the `x` range will be linearly extrapolated using passed slopes."""
     res = np.interp(x_new, x, y)
     for i, curr_x in enumerate(x_new):
         if curr_x < x[0]:
@@ -14,6 +18,16 @@ def interpolate(x_new, x, y, left_slope, right_slope):
 
 
 class interp1d:
+    """Return a 1d piecewise linear interpolant to a function defined by pairs of data points `(x, y)`. Function values
+    at points outside the `x` range will be linearly extrapolated.
+
+    Parameters
+    ----------
+    x : 1d array-like
+        X coordinates of function values.
+    y : 1d array-like
+        Function values, evaluated at `x`. Must match the length of `x`.
+    """
     def __init__(self, x, y):
         x = np.array(x)
         y = np.array(y)
@@ -29,6 +43,18 @@ class interp1d:
         self.right_slope = (self.y[-1] - self.y[-2]) / (self.x[-1] - self.x[-2])
 
     def __call__(self, x):
+        """Evaluate the interpolant at passed coordinates `x`.
+
+        Parameters
+        ----------
+        x : 1d array-like
+            Points to evaluate the interpolant at.
+
+        Returns
+        -------
+        y : 1d array-like
+            Interpolated values, matching the length of `x`.
+        """
         x = np.array(x)
         is_scalar_input = (x.ndim == 0)
         res = interpolate(x.ravel(), self.x, self.y, self.left_slope, self.right_slope)
