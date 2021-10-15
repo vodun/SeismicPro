532085c0b394691db9bddfa1fc3220f94dcc0333
Vadim Sobolevskiy <werever@mail.ru>
2021-10-15 09:40:34 +0000

    add decorators

--- seismicpro/src/cropped_gather.py
+++ seismicpro/src/cropped_gather.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .decorators import batch_method
+
 class CroppedGazer:
     ''' class contain '''
     def __init__(
@@ -40,40 +42,44 @@ class CroppedGazer:
     def make_crops(self):
         # two ways: save to list or save to numpy array
         # using list now
+        # make_model_inputs() ?
         print('start make_crops()')
         crops = []
         data = self.load_data()
         coords = np.array(self.origin, dtype=int).reshape(-1, 2)
-        print('make_crops, origins', coords)
+        # print('make_crops, origins', coords)
         for i in range(coords.shape[0]):
             crops.append(self.make_single_crop(coords[i], data))
         return crops
 
 
     def make_single_crop(self, origin, data):
-        print('start make_single_crop()')
+        # print('start make_single_crop()')
         shapes = self.parent_shape
         crop_size = self.crop_size
         tuple_crop_pad = self.to_tuple(self.crop_pad)
 
-        print(f'origin: {origin}, padding: {self.crop_pad}, crop_pad: {crop_pad}, crop_size: {crop_size}, shapes: {shapes}')
+        # print(f'origin: {origin}, padding: {self.crop_pad}, crop_pad: {tuple_crop_pad}, crop_size: {crop_size}, shapes: {shapes}')
 
         start_x, start_y = origin[1] + tuple_crop_pad[1][0], origin[0] + tuple_crop_pad[0][0]
         dx, dy = crop_size[1] - sum(tuple_crop_pad[1]), crop_size[0] - sum(tuple_crop_pad[0])
-        print(start_x, dx)
+        # print(start_x, dx)
         if start_x + dx > shapes[1]:
             start_x = shapes[1] - dx
         if start_y + dy > shapes[0]:
             start_y = shapes[0] - dy
 
-        print('Cutting shape is', (start_y, start_x, start_y+dy, start_x+dx))
+        # print('Cutting shape is', (start_y, start_x, start_y+dy, start_x+dx))
         if self.crop_pad:
             return np.pad(data[start_y:start_y+dy, start_x:start_x+dx], tuple_crop_pad, constant_values=self.crop_fill)
         else:
             return data[start_y:start_y+dy, start_x:start_x+dx]
 
+
+    @batch_method(target='for')
     def assembly_gather(self):
         # do not support a padding
+        print('start assembly_gather()')
         result = np.zeros(shape=self.parent_shape)
         mask = np.zeros(shape=self.parent_shape) # change to np.zeros !!!
         for origin, crop in zip(self.origin, self.crops):
@@ -122,10 +128,12 @@ class CroppedGazer:
 
     def origins_from_str(self, crop_rule):
         print('start origins_from_str()')
-        if crop_rule == 'random':  # from uniform distribution. Return 
-            return (np.random.randint(self.parent_shape[0] + 2 * self.gather_pad[0] - self.crop_size[0]), 
-                    np.random.randint(self.parent_shape[1] + 2 * self.gather_pad[1] - self.crop_size[1]))
-        elif crop_rule == 'grid':
+        tuple_gather_pad = self.to_tuple(self.gather_pad)
+        print(tuple_gather_pad, self.gather_pad, self.parent_shape)
+        if crop_rule == 'random':  # from uniform distribution. 
+            return (np.random.randint(self.parent_shape[0] + sum(tuple_gather_pad[0]) - self.crop_size[0]), 
+                    np.random.randint(self.parent_shape[1] + sum(tuple_gather_pad[1]) - self.crop_size[1]))
+        elif crop_rule == 'grid': # do not support padding
             print('x_range', 0, self.parent_shape[0], self.crop_size[0])
             origin_x = np.arange(0, self.parent_shape[0], self.crop_size[0])
             print('x_range', 0, self.parent_shape[1], self.crop_size[1])
