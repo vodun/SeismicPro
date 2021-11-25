6e1af67a72def863d83938fcc5e2c16a3dbddc8b
Vadim Sobolevskiy <werever@mail.ru>
2021-11-25 16:13:26 +0000

    return old aggregation

--- seismicpro/src/cropped_gather.py
+++ seismicpro/src/cropped_gather.py
@@ -6,12 +6,12 @@ from .decorators import batch_method
 class CroppedGather:
     ''' cool docstring here '''
 
-    def __init__(self, gather, shape, origins, pad_mode='constant'):
+    def __init__(self, gather, crop_shape, origins, pad_kwargs={}):
         self.gather = gather
-        self.crop_shape = shape
+        self.crop_shape = crop_shape
         self.n_origins = origins.shape[0]
         self.origins = origins
-        self.crops = self.make_crops(self._padding(pad_mode))
+        self.crops = self.make_crops(self._padding(pad_kwargs))
 
     def make_crops(self, data):
         ''' TODO: docs '''
@@ -21,20 +21,18 @@ class CroppedGather:
             crops[i] = data[start_x:start_x + dx, start_y:start_y + dy]
         return crops
 
-    def _padding(self, pad_mode):
+    def _padding(self, pad_kwargs):
         '''Checking if crop window is out of the gather and pad gather to get crops. '''
         max_shapes = np.atleast_2d(self.origins.max(axis=0))
         temp_max = (max_shapes + self.crop_shape - self.gather.shape).max(axis=0)
         pad_shape_x, pad_shape_y = np.maximum(np.zeros(shape=2, dtype=int), temp_max)
-        return np.pad(self.gather.data, ((0, pad_shape_x), (0, pad_shape_y)), mode=pad_mode)
+        return np.pad(self.gather.data, ((0, pad_shape_x), (0, pad_shape_y)), **pad_kwargs)
 
     @batch_method(target='for')
-    def assemble_gather(self, input_data=None, strategy='greed', aggregation_mode='mean'):
+    def assemble_gather(self, aggregation_mode='mean'):
         ''' TODO: docs '''
         # test save_to in predict_model
-        assembling_data = self._assembling(self.crops if input_data is None else input_data, 
-                                           strategy=strategy, aggregation_mode=aggregation_mode)
-
+        assembling_data = self._assembling(self.crops, aggregation_mode=aggregation_mode)
         # avoiding gather data copying 
         rest_data = self.gather.data
         self.gather.data = None
@@ -44,53 +42,19 @@ class CroppedGather:
         gather.data = assembling_data
         return gather
 
-    def _assembling(self, data, strategy=None, aggregation_mode=None):
+    def _assembling(self, data, aggregation_mode):
         ''' TODO: docs ''' 
-        result = np.full(shape=(*self.gather.shape, 1), fill_value=np.nan, dtype=float)
-        mask = np.full(shape=self.gather.shape, fill_value=-1, dtype=int)
+        result = np.zeros(shape=self.gather.shape, dtype=float)
+        mask = np.zeros(shape=self.gather.shape, dtype=int)
+        for i, origin in enumerate(self.origins):
+            result[origin[0]:origin[0] + self.crop_shape[0], origin[1]:origin[1] + self.crop_shape[1]] += data[i]
+            mask[origin[0]:origin[0] + self.crop_shape[0], origin[1]:origin[1] + self.crop_shape[1]] += 1
+        result = self._aggregate(result, mask, mode=aggregation_mode)
+        return result
 
-        mask_add = 1
-        if strategy == 'rand':
-            rand_i = np.random.permutation(np.arange(self.n_origins))
-            for i in rand_i:
-                one_crop = data[i]
-                origin = self.origins[i]
-                # index where I want to write next crop
-                plate_index = mask[origin[0]:origin[0] + self.crop_shape[0], origin[1]:origin[1] + self.crop_shape[1]].max() + 1
-                # checking if result array need to extend by axis=2
-                if plate_index >= result.shape[2]:
-                    result = np.dstack((result, np.full(shape=(*self.gather.shape, 1), fill_value=np.nan, dtype=float)))
-                result[origin[0]:origin[0] + self.crop_shape[0], origin[1]:(origin[1] + self.crop_shape[1]), plate_index] = one_crop
-                mask[origin[0]:origin[0] + self.crop_shape[0], origin[1]:origin[1] + self.crop_shape[1]] = plate_index
-            print('Rand aggregate matrix shape:', result.shape)
-
-        # based on origins structure
-        # greed strategy at 3 times faster and aggregate matrix(result) 2 times smaller
-        if strategy == 'greed':
-            masked_origins = np.ma.array(self.origins, mask=False)
-            level = 0
-            while masked_origins.mask.sum() < self.origins.size:
-                border_x, border_y = 0, 0
-                for origin in masked_origins:
-                    if (origin.mask == False).all():
-                        if origin[0] >= border_x or origin[1] >= border_y:
-                            # getting index of origin to get crop
-                            i, = np.where(np.all(self.origins == origin, axis=1))
-                            one_crop = data[i]
-                            result[origin[0]:origin[0] + self.crop_shape[0], origin[1]:(origin[1] + self.crop_shape[1]), level] = one_crop
-                            border_x = origin[0] + self.crop_shape[0]
-                            border_y = origin[1] + self.crop_shape[1]
-                            masked_origins.mask[i] = True
-                result = np.dstack((result, np.full(shape=(*self.gather.shape, 1), fill_value=np.nan, dtype=float)))
-                level += 1
-            print('Greed aggregate matrix shape:', result.shape)
-
-
-        return self._aggregate(result, mode=aggregation_mode)
-
-    def _aggregate(self, data, mode):
+    def _aggregate(self, data, mask, mode):
         ''' TODO: docs '''
         if mode == 'mean':
-            return np.nanmean(data, axis=2)
+            return data / mask
         else:
             raise NotImplementedError('Using mode are not implement now.')
