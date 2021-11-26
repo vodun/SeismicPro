4adcdecb29d89c60c6e82f6a328ccfad72bc5d79
anton-br <ziliboba753@gmail.com>
2021-11-26 18:48:15 +0300

    fix data shapes in batch test

--- seismicpro/src/tests/batch_test.py
+++ seismicpro/src/tests/batch_test.py
@@ -21,10 +21,10 @@ def test_batch_load_combined(segy_path):
     """test_batch_load_combined"""
     survey = Survey(segy_path, header_index='TRACE_SEQUENCE_FILE', name='raw')
     dataset = SeismicDataset(surveys=survey)
-    batch = dataset.next_batch(1000)
+    batch = dataset.next_batch(200)
     batch = batch.load(src='raw', combined=True)
     assert len(batch.raw) == 1
-    assert len(batch.raw[0].data) == 1000
+    assert len(batch.raw[0].data) == 200
 
 def test_batch_make_model_inputs(dataset):
     """test_batch_make_model_inputs"""
@@ -33,7 +33,7 @@ def test_batch_make_model_inputs(dataset):
     batch = dataset.next_batch(1, shuffle=False)
     batch.load(src='raw').make_model_inputs(src=[batch.raw[0].data], dst='inputs', mode='c', axis=0,
                                             expand_dims_axis=1)
-    assert batch.inputs.shape == (126, 1, 1500)
+    assert batch.inputs.shape == (36, 1, 1500)
 
 def test_batch_make_model_outputs(dataset):
     """test_batch_make_model_outputs"""
@@ -42,6 +42,6 @@ def test_batch_make_model_outputs(dataset):
     batch = dataset.next_batch(2, shuffle=False)
     batch.load(src='raw').make_model_inputs(src=[batch.raw[0].data, batch.raw[1].data], dst='inputs', mode='c', axis=0,
                                             expand_dims_axis=1)
-    batch.split_model_outputs(src='inputs', dst='outputs', shapes=[126, 198])
-    assert batch.outputs[0].shape == (126, 1, 1500)
-    assert batch.outputs[1].shape == (198, 1, 1500)
+    batch.split_model_outputs(src='inputs', dst='outputs', shapes=[36, 36])
+    assert batch.outputs[0].shape == (36, 1, 1500)
+    assert batch.outputs[1].shape == (36, 1, 1500)
