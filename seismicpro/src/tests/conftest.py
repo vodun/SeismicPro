640ab6f4c67a551cd30b6126f5f4c54bc9a8858d
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-12-23 19:56:18 +0300

    Minor fixes in main conftest

--- seismicpro/src/tests/conftest.py
+++ seismicpro/src/tests/conftest.py
@@ -1,17 +1,15 @@
-"""Implements fixtures that """
-
-import os
+"""Generate a test SEG-Y file"""
 
 import pytest
 
-from seismicpro.src import make_prestack_segy
+from seismicpro import make_prestack_segy
 
 
-@pytest.fixture(scope='package', autouse=True)
+@pytest.fixture(scope="package", autouse=True)
 def segy_path(tmp_path_factory):
-    """ Fixture that creates segy file """
-    path = os.path.join(tmp_path_factory.getbasetemp(), 'test_prestack.sgy')
-    make_prestack_segy(path, survey_size=(300, 300), origin=(0, 0), sources_step=(50, 300), recievers_step=(100, 25),
-                       bin_size=(50, 50), activation_dist=(500, 500), n_samples=1500, sample_rate=2000, delay=0,
-                       trace_gen=None)
+    """Create a temporary SEG-Y file with randomly generated traces."""
+    path = tmp_path_factory.mktemp("data") / "test_prestack.sgy"
+    make_prestack_segy(path, survey_size=(300, 300), origin=(0, 0), sources_step=(50, 150), recievers_step=(100, 25),
+                       bin_size=(50, 50), activation_dist=(200, 200), n_samples=1000, sample_rate=2000, delay=0,
+                       bar=False, trace_gen=None)
     return path
