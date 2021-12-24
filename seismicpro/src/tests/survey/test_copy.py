e5baace415cde9a61b5141ddbd045327f27cfe42
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-12-24 14:16:10 +0300

    Add test_copy docs

--- seismicpro/src/tests/survey/test_copy.py
+++ seismicpro/src/tests/survey/test_copy.py
@@ -8,7 +8,10 @@ from . import assert_surveys_equal
 
 
 class TestCopy:
+    """Test Survey copying and serialization."""
+
     def test_pickle(self, survey, tmp_path):
+        """Test that survey picking-unpickling does not change the survey."""
         dump_path = tmp_path / "survey"
         with open(dump_path, "wb") as f:
             pickle.dump(survey, f)
@@ -17,10 +20,12 @@ class TestCopy:
         assert_surveys_equal(survey, survey_restored)
 
     def test_copy(self, survey):
+        """Test whether survey copy equals to the survey itself."""
         survey_copy = survey.copy()
         assert_surveys_equal(survey, survey_copy)
 
     def test_is_copy_deep(self, survey):
+        """Test whether `survey.copy` returns a deepcopy by changing some of its mutable attributes."""
         survey_copy = survey.copy()
         survey_copy.headers = survey_copy.headers.iloc[1:]
         assert not survey.headers.equals(survey_copy.headers)
