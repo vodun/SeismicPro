74ceb3c908d16e195e48d5aa503868fcbe191f35
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2021-12-24 15:54:29 +0300

    Add test_reindex docs

--- seismicpro/src/tests/survey/test_reindex.py
+++ seismicpro/src/tests/survey/test_reindex.py
@@ -7,8 +7,11 @@ from . import assert_surveys_equal, assert_survey_processed_inplace
 
 
 class TestReindex:
+    """Test Survey reindexation."""
+
     @pytest.mark.parametrize("inplace", [True, False])
-    def test_reindex_to_same_index(self, survey, inplace):
+    def test_reindex_to_current_index(self, survey, inplace):
+        """Reindexation to the current index must return the same survey."""
         index = survey.headers.index.names
         survey_reindexed = survey.reindex(index, inplace=inplace)
         assert_surveys_equal(survey, survey_reindexed)
@@ -16,6 +19,7 @@ class TestReindex:
 
     @pytest.mark.parametrize("new_index", ["CDP", ["CDP_X", "CDP_Y"]])
     def test_reindex_and_back(self, survey, new_index):
+        """Reindexation to a new index and back must return the same survey."""
         index = survey.headers.index.names
         survey_reindexed = survey.reindex(new_index, inplace=False).reindex(index, inplace=False)
         assert_surveys_equal(survey, survey_reindexed, ignore_column_order=True, ignore_dtypes=True)
@@ -23,6 +27,7 @@ class TestReindex:
     @pytest.mark.parametrize("first_index", ["TRACE_SEQUENCE_FILE", ["CDP_X", "CDP_Y"]])
     @pytest.mark.parametrize("second_index", ["CDP", ["FieldRecord", "TraceNumber"]])
     def test_consecutive_reindex(self, survey, first_index, second_index):
+        """Several consequent reindexations must be equal to reindexing to the last one specified."""
         survey_first_second = survey.reindex(first_index, inplace=False).reindex(second_index, inplace=False)
         survey_second = survey.reindex(second_index, inplace=False)
         assert_surveys_equal(survey_first_second, survey_second, ignore_column_order=True, ignore_dtypes=True)
@@ -30,6 +35,12 @@ class TestReindex:
     @pytest.mark.parametrize("new_index", ["TRACE_SEQUENCE_FILE", ["CDP_X", "CDP_Y"]])
     @pytest.mark.parametrize("inplace", [True, False])
     def test_reindex(self, survey, new_index, inplace):
+        """Validate `reindex`:
+        1. Requested index is set,
+        2. The index in monotonically increasing,
+        3. The set of loaded SEG-Y headers has not changed,
+        4. Values of trace headers has not changed, only their order in `survey.headers`.
+        """
         survey_copy = survey.copy()
         survey_headers = set(survey.headers.index.names) | set(survey.headers.columns)
         survey_reindexed = survey.reindex(new_index, inplace=inplace)
