a3e74313ef57bcfc6426762254ce4a35cb3e2f75
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2022-02-16 08:38:21 +0000

    Fix comments

--- tutorials/utils.py
+++ tutorials/utils.py
@@ -3,18 +3,18 @@
 import numpy as np
 from scipy import signal
 
-def generate_trace(reflection_event_time=(10,700,1200), reflection_event_amplitude=(6,-12,8),
-                    nmo_velocity=(1.6,2.,2.4), wavelet_lenght=50, wavelet_width=5, **kwargs):
+def generate_trace(reflection_event_times=(20, 1400, 2400), reflection_event_amplitude=(6.0, -12.0, 8.0),
+                   nmo_velocity=(1.6, 2.0, 2.4), wavelet_lenght=50, wavelet_width=5, **kwargs):
 
-    """Generates a seismic trace using reflectivity parameters and trace's headers.
+    """Generates a seismic trace using reflectivity parameters and trace's headers with Ricker impulse.
 
     Parameters
     ----------
-    reflection_event_time : 1d-darray, defaults to (10, 700, 1200)
-        Zero-offset times of a reflection events measured in samples.
-    reflection_event_amplitude : 1d-darray, defaults to (6,-12,8)
+    reflection_event_time : 1d-darray, defaults to (20, 1400, 2400)
+        Zero-offset times of reflection events measured in ms.
+    reflection_event_amplitude : 1d-darray, defaults to (6.0, -12.0, 8.0)
         Amplitudes of reflection events.
-    nmo_velocity : 1d-darray, defaults to (1.6, 2., 2.4)
+    nmo_velocity : 1d-darray, defaults to (1.6, 2.0, 2.4)
         NMO velocities for the reflection events, m/ms.
     wavelet_lenght : int, defaults to 50
         Overall lenght of the vector with Ricker vawelet. Equivalent of `points` parameter of `scipy.signal.ricker`.
@@ -29,20 +29,22 @@ def generate_trace(reflection_event_time=(10,700,1200), reflection_event_amplitu
         Generated seismic trace.
     """
 
-    # inline3d = kwargs.get('INLINE_3D')
     n_samples = kwargs.get('TRACE_SAMPLE_COUNT')
     sample_rate = kwargs.get('TRACE_SAMPLE_INTERVAL')
     offset = kwargs.get('offset')
-    times = np.array(reflection_event_time)
+    times = np.array(reflection_event_times) / (sample_rate / 1000) # cast to samples
     reflections = np.array(reflection_event_amplitude)
     velocities = np.array(nmo_velocity)
+    
+    equal_lengths = (len(times) == len(reflections) == len(velocities))
+    if not equal_lengths:
+        raise ValueError("'reflection_event_times', 'reflection_event_amplitude' and 'nmo_velocity' should have equal lengths")
 
     # Inversed normal moveout
     shifted_times = ((times**2 + offset**2 / (velocities * (sample_rate / 1000))**2)**0.5).astype(int)
     ref_series = np.zeros(max(n_samples, max(shifted_times)) + 1)
 
-    # Tweak reflectivity of traces based in indline3D header to make Survey and Gathers statistics differ
-    # reflections = reflections * (1 + 10 / (10 + inline3d))
+    # Tweak reflection event amplitudes to make Survey and Gathers statistics differ
     reflections = np.random.normal(reflections, np.abs(reflections) / 5)
 
     ref_series[shifted_times] = reflections
