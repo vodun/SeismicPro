cb529fe80b86c4967095c7cd96852de0e9edbcd7
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2022-02-28 17:51:39 +0300

    Unify all interactive plot classes

--- seismicpro/src/utils/interactive_plot_utils.py
+++ seismicpro/src/utils/interactive_plot_utils.py
@@ -41,29 +41,48 @@ TITLE_STYLE = "<style>p{word-wrap:normal; text-align:center; font-size:14px}</st
 TITLE_TEMPLATE = "{style} <b><p>{title}</p></b>"
 
 
-class InteractivePlot:
-    def __init__(self, plot_fn=None, click_fn=None, allow_unclick=True, unclick_fn=None, init_click_coords=None,
-                 marker_params=None, title="", figsize=(4.5, 4.5), toolbar_position="left"):
-        self.plot_fn = plot_fn
-        self.click_fn = click_fn
-        self.unclick_fn = unclick_fn
-        self.is_clickable = click_fn is not None
+def align_args(reference_arg, *args):
+    from .general_utils import to_list
+    reference_arg = to_list(reference_arg)
+    processed_args = []
+    for arg in args:
+        arg = to_list(arg)
+        if len(arg) == 1:
+            arg *= len(reference_arg)
+        if len(arg) != len(reference_arg):
+            raise ValueError("Lengths of all passed arguments must match")
+        processed_args.append(arg)
+    return reference_arg, *processed_args
 
-        marker_params = {} if marker_params is None else marker_params
-        self.marker_params = {"color": "black", "marker": "+", **marker_params}
+
+class InteractivePlot:
+    def __init__(self, *, plot_fn=None, click_fn=None, unclick_fn=None, marker_params=None, title="",
+                 init_click_coords=None, toolbar_position="left", figsize=(4.5, 4.5)):
+        list_args = align_args(plot_fn, click_fn, unclick_fn, marker_params, title)
+        self.plot_fn_list, self.click_fn_list, self.unclick_fn_list, marker_params_list, self.title_list = list_args
+        self.marker_params_list = []
+        for params in marker_params_list:
+            if params is None:
+                params = {}
+            params = {"color": "black", "marker": "+", **params}
+            self.marker_params_list.append(params)
+
+        self.n_views = len(self.plot_fn_list)
+        self.current_view = 0
+
         self.click_time = None
         self.click_marker = None
         self.init_click_coords = init_click_coords
 
         # Construct a figure
+        self.toolbar_position = toolbar_position
+        if toolbar_position is None:
+            toolbar_position = "left"
         with plt.ioff():
             # Add tight_layout to always correctly show colorbar ticks
             self.fig, self.ax = plt.subplots(figsize=figsize, tight_layout=True)  # pylint: disable=invalid-name
-        toolbar_visible = (toolbar_position is not None)
-        if toolbar_position is None:
-            toolbar_position = "left"
         self.fig.canvas.header_visible = False
-        self.fig.canvas.toolbar_visible = toolbar_visible
+        self.fig.canvas.toolbar_visible = False
         self.fig.canvas.toolbar_position = toolbar_position
 
         # Setup event handlers
@@ -72,33 +91,80 @@ class InteractivePlot:
         if self.is_clickable:
             self.fig.canvas.mpl_connect("button_press_event", self.on_click)
             self.fig.canvas.mpl_connect("button_release_event", self.on_release)
-            if allow_unclick:
-                self.fig.canvas.mpl_connect("key_press_event", self.on_press)
+            self.fig.canvas.mpl_connect("key_press_event", self.on_press)
 
         # Build plot box
-        title = TITLE_TEMPLATE.format(style=TITLE_STYLE, title=title)
-        self.title = widgets.HTML(value=title, layout=widgets.Layout(**TEXT_LAYOUT))
-        self.header = self.create_header()
-        self.box = widgets.VBox([self.header, self.fig.canvas])
+        self.title_widget = widgets.HTML(value="", layout=widgets.Layout(**TEXT_LAYOUT))
+        self.view_button = widgets.Button(icon="exchange", layout=widgets.Layout(**BUTTON_LAYOUT))
+        self.view_button.on_click(self.on_view_toggle)
+        self.header = self.construct_header()
+        self.toolbar = self.construct_toolbar()
+        self.box = self.construct_box()
 
     def __del__(self):
         del self.fig.interactive_plotter
         plt.close(self.fig)
 
-    def create_header(self):
-        placeholder = widgets.HTML(layout=widgets.Layout(**BUTTON_LAYOUT))
-        if self.fig.canvas.toolbar_position == "left":
-            return widgets.HBox([placeholder, self.title])
-        if self.fig.canvas.toolbar_position == "right":
-            return widgets.HBox([self.title, placeholder])
-        return self.title
+    @property
+    def plot_fn(self):
+        return self.plot_fn_list[self.current_view]
+
+    @property
+    def click_fn(self):
+        return self.click_fn_list[self.current_view]
+
+    @property
+    def is_clickable(self):
+        return self.click_fn is not None
+
+    @property
+    def unclick_fn(self):
+        return self.unclick_fn_list[self.current_view]
+
+    @property
+    def is_unclickable(self):
+        return self.unclick_fn is not None
+
+    @property
+    def marker_params(self):
+        return self.marker_params_list[self.current_view]
+
+    @property
+    def title(self):
+        return self.title_list[self.current_view]
+
+    def construct_buttons(self):
+        if self.n_views == 1:
+            return []
+        return [self.view_button]
+
+    def construct_header(self):
+        buttons = self.construct_buttons()
+        if self.toolbar_position is not None:
+            buttons = []
+        return widgets.HBox([*buttons, self.title_widget])
+
+    def construct_toolbar(self):
+        toolbar = self.fig.canvas.toolbar
+        if self.toolbar_position in {"top", "bottom"}:
+            toolbar.orientation = "horizontal"
+            return widgets.HBox([*self.construct_buttons(), toolbar])
+        return widgets.VBox([*self.construct_buttons(), toolbar])
+
+    def construct_box(self):
+        titled_box = widgets.HBox([widgets.VBox([self.header, self.fig.canvas])])
+        if self.toolbar_position == "top":
+            return widgets.VBox([self.toolbar, titled_box])
+        if self.toolbar_position == "bottom":
+            return widgets.VBox([titled_box, self.toolbar])
+        if self.toolbar_position == "left":
+            return widgets.HBox([self.toolbar, titled_box])
+        if self.toolbar_position == "right":
+            return widgets.HBox([titled_box, self.toolbar])
+        return titled_box
 
     def _resize(self, width):
-        # Remove this correction when fixed in ipympl
-        width = width / self.fig.canvas._dpi_ratio    # pylint: disable=protected-access
         width += 4  # Correction for main axes margins
-        if self.fig.canvas.toolbar_visible and (self.fig.canvas.toolbar_position in {"left", "right"}):
-            width += 44  #  Correction for an optional toolbar and its margins
         self.header.layout.width = f"{int(width)}px"
 
     def on_resize(self, event):
@@ -128,40 +194,51 @@ class InteractivePlot:
             self.click_time = None
             self._click((event.xdata, event.ydata))
 
+    def _unclick(self):
+        if self.click_marker is None:
+            return
+        self.unclick_fn()
+        self.click_marker.remove()
+        self.click_marker = None
+        self.fig.canvas.draw_idle()  # TODO: switch to blit
+
     def on_press(self, event):
         if (event.inaxes != self.ax) or (event.key != "escape"):
             return
-        if self.click_marker is not None:
-            if self.unclick_fn is not None:
-                self.unclick_fn()
-            self.click_marker.remove()
-            self.click_marker = None
-            self.fig.canvas.draw_idle()  # TODO: switch to blit
+        self._unclick()
+
+    def set_view(self, view):
+        if view < 0 or view >= self.n_views:
+            raise ValueError("Unknown view")
+        self.ax.clear()
+        self.current_view = view
+        self.redraw()
+
+    def on_view_toggle(self, event):
+        _ = event
+        if self.is_unclickable:
+            self._unclick()
+        self.set_view((self.current_view + 1) % self.n_views)
 
     def set_title(self, title):
-        self.title.value = TITLE_TEMPLATE.format(style=TITLE_STYLE, title=title)
+        self.title_widget.value = TITLE_TEMPLATE.format(style=TITLE_STYLE, title=title)
 
-    def plot(self, display_box=True):
-        self._resize(self.fig.get_figwidth() * self.fig.dpi)  # Init the width of the box
+    def redraw(self):
         if self.plot_fn is not None:
             self.plot_fn(ax=self.ax)
+        title = self.title
+        if callable(title):
+            title = title()
+        self.set_title(title)
+
+    def plot(self, display_box=True):
+        self.redraw()
         if self.is_clickable and self.init_click_coords is not None:
             self._click(self.init_click_coords)
+        # Init the width of the box
+        self._resize(self.fig.get_figwidth() * self.fig.dpi / self.fig.canvas.device_pixel_ratio)
         if display_box:
             display(self.box)
-
-
-class ToggleClickablePlot(InteractivePlot):
-    def __init__(self, *args, toggle_fn=None, toggle_icon=None, **kwargs):
-        self.button = widgets.Button(icon=toggle_icon, layout=widgets.Layout(**BUTTON_LAYOUT))
-        self.button.on_click(toggle_fn)
-        super().__init__(*args, **kwargs)
-
-    def create_header(self):
-        placeholder = widgets.HTML(layout=widgets.Layout(**BUTTON_LAYOUT))
-        if self.fig.canvas.toolbar_position == "right":
-            return widgets.HBox([self.button, placeholder])
-        return widgets.HBox([self.button, self.title])
 
 
 class PairedPlot:
