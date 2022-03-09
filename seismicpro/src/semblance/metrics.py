Fix views of signal leakage metric

seismicpro/src/semblance/metrics.py
@@ -25,7 +25,7 @@ class SignalLeakage(PipelineMetric):
     min_value = 0
     max_value = None
     is_lower_better = True
-    views = ("plot_gathers", "plot_semblances")
+    views = ("plot_diff_gather", "plot_diff_semblance")
     args_to_unpack = ("gather_before", "gather_after")
 
     @staticmethod
@@ -34,14 +34,8 @@ class SignalLeakage(PipelineMetric):
         gather_diff.data = gather_after.data - gather_before.data
         semblance_diff = gather_diff.calculate_semblance(velocities)
         semblance_before = gather_before.calculate_semblance(velocities)
-        return np.max(semblance_diff.ptp(axis=1) / (1 - 1e-10 - semblance_before.ptp(axis=1)))
-
-    @pass_calc_args
-    def plot_diff_semblance(gather_before, gather_after, velocities, ax, **kwargs):
-        gather_diff = gather_before.copy(ignore=["data", "headers", "samples"])
-        gather_diff.data = gather_after.data - gather_before.data
-        semblance_diff = gather_diff.calculate_semblance(velocities)
-        semblance_diff.plot(ax=ax, **kwargs)
+        signal_leakage = semblance_diff.semblance.ptp(axis=1) / (1 + 1e-6 - semblance_before.semblance.ptp(axis=1))
+        return max(0, np.max(signal_leakage))
 
     @pass_calc_args
     def plot_diff_gather(gather_before, gather_after, velocities, ax, **kwargs):
@@ -49,3 +43,10 @@ class SignalLeakage(PipelineMetric):
         gather_diff = gather_before.copy(ignore=["data", "headers", "samples"])
         gather_diff.data = gather_after.data - gather_before.data
         gather_diff.plot(ax=ax, **kwargs)
+
+    @pass_calc_args
+    def plot_diff_semblance(gather_before, gather_after, velocities, ax, **kwargs):
+        gather_diff = gather_before.copy(ignore=["data", "headers", "samples"])
+        gather_diff.data = gather_after.data - gather_before.data
+        semblance_diff = gather_diff.calculate_semblance(velocities)
+        semblance_diff.plot(ax=ax, **kwargs)