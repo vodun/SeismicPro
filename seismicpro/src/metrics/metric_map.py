2bec5fcfcdcc45b927532262262f377a2c63ad26
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2022-03-18 21:28:31 +0300

    Add metric map docs

--- seismicpro/src/metrics/metric_map.py
+++ seismicpro/src/metrics/metric_map.py
@@ -11,6 +11,10 @@ from ..utils import to_list, add_colorbar, set_ticks, set_text_formatting
 
 
 class BaseMetricMap:
+    """Base metric map class. Implements general input processing, map visualization and reaggregation methods.
+
+    Should not be instantiated directly, use `MetricMap` or its subclasses instead.
+    """
     def __init__(self, coords, metric_values, *, coords_cols=None, metric=None, metric_name=None, agg=None):
         from .metrics import Metric, PartialMetric  # pylint: disable=import-outside-toplevel
         if metric is None:
@@ -41,24 +45,29 @@ class BaseMetricMap:
         self.agg = agg
 
     def __getattr__(self, name):
+        """Redirect attribute search into metric class or instance."""
         return getattr(self.metric, name)
 
     @property
     def plot_title(self):
+        """str: title of the map plot."""
         agg_name = self.agg.__name__ if callable(self.agg) else self.agg
         return f"{agg_name}({self.metric_name})"
 
     @property
     def x_tick_labels(self):
+        """None or array-like: labels of x axis ticks."""
         return None
 
     @property
     def y_tick_labels(self):
+        """None or array-like: labels of y axis ticks."""
         return None
 
     @plotter(figsize=(10, 7))
     def _plot(self, *, title=None, x_ticker=None, y_ticker=None, is_lower_better=None, vmin=None, vmax=None, cmap=None,
               colorbar=True, center_colorbar=True, clip_threshold_quantile=0.95, keep_aspect=False, ax=None, **kwargs):
+        """Plot the metric map."""
         is_lower_better = self.is_lower_better if is_lower_better is None else is_lower_better
         # Handle plain Metric case
         vmin = vmin or self.vmin or self.min_value
@@ -91,6 +100,54 @@ class BaseMetricMap:
         set_ticks(ax, "y", self.coords_cols[1], self.y_tick_labels, **y_ticker)
 
     def plot(self, *, interactive=False, plot_on_click=None, **kwargs):
+        """Plot the metric map.
+
+        Parameters
+        ----------
+        title : str, optional
+            Map plot title. If not given, a default title with metric name, aggregation function and binarization info
+            is shown.
+        x_ticker : dict, optional
+            Parameters for ticks and ticklabels formatting for the x-axis; see `.utils.set_ticks` for more details.
+        y_ticker : dict, optional
+            Parameters for ticks and ticklabels formatting for the y-axis; see `.utils.set_ticks` for more details.
+        is_lower_better : bool or None, optional
+            Specifies if lower value of the metric is better. Affects the default colormap. Taken from `metric` if not
+            given.
+        vmin : float or None, optional
+            Minimum colorbar value. Taken from `metric` if not given.
+        vmax : float or None, optional
+            Maximum colorbar value. Taken from `metric` if not given.
+        cmap : str or matplotlib.colors.Colormap, optional
+            Map colormap. If not given defined by `is_lower_better`: if it is `bool`, a green-red colormap is used,
+            if `None` - "coolwarm".
+        colorbar : bool or dict, optional, defaults to True
+            Whether to add a colorbar to the right of the metric map plot. If `dict`, defines extra keyword arguments
+            for `matplotlib.figure.Figure.colorbar`.
+        center_colorbar : bool, optional, defaults to True
+            Whether to center the colorbar around mean metric value if `is_lower_better` is `None`.
+        clip_threshold_quantile : float, optional, defaults to 0.95
+            Clip metric values on the colorbar whose deviation from metric mean is greater than that defined by the
+            quantile. Has an effect only if `center_colorbar` is `True` and `is_lower_better` is `None`.
+        keep_aspect : bool, optional, defaults to False
+            Whether to keep aspect ratio of the map plot.
+        ax : matplotlib.axes.Axes, optional, defaults to None
+            Axes of the figure to plot on.
+        kwargs : misc, optional
+            Any additional arguments for `matplotlib.axes.Axes.scatter` or `matplotlib.axes.Axes.imshow` depending on
+            the map type.
+        interactive : bool, optional, defaults to `False`
+            Whether to plot metric map in interactive mode. Clicking on the map will result in displaying the `views`
+            defined by the map metric. If no views are implemented, `plot_on_click` argument must be specified. Note
+            that if `Metric.get_views` expects any arguments, they must be passed to `kwargs`, see docs of the metric
+            used for more details. Interactive plotting must be performed in a JupyterLab environment with the the
+            `%matplotlib widget` magic executed and `ipympl` and `ipywidgets` libraries installed.
+        plot_on_click : callable or list of callable, optional, only for interactive mode
+            Views called on each click to display some data representation at the click location. Each of them must
+            accept click coordinates as a tuple as `coords` argument and axes to plot on as `ax` argument.
+        plot_on_click_kwargs : dict or list of dict, optional, only for interactive mode
+            Any additional arguments for each view.
+        """
         if not interactive:
             return self._plot(**kwargs)
 
@@ -107,17 +164,36 @@ class BaseMetricMap:
         return self.interactive_map_class(self, plot_on_click=plot_on_click_list, **kwargs).plot()
 
     def aggregate(self, agg=None, bin_size=None):
+        """Aggregate the map with new `agg` and `bin_size`.
+
+        agg : str or callable, optional
+            A function used for aggregating the map. If not given, will be determined by the value of `is_lower_better`
+            attribute of the metric class in order to highlight outliers. Passed directly to
+            `pandas.core.groupby.DataFrameGroupBy.agg`.
+        bin_size : int, float or array-like with length 2, optional
+            Bin size for X and Y axes. If single `int` or `float`, the same bin size will be used for both axes.
+
+        Returns
+        -------
+        metrics_maps : BaseMetricMap
+            Aggregated map.
+        """
         return self.map_class(self.metric_data[self.coords_cols], self.metric_data[self.metric_name],
                               metric=self.metric, agg=agg, bin_size=bin_size)
 
 
 class ScatterMap(BaseMetricMap):
+    """Construct a map by aggregating metric values defined for the same coordinates using `agg`. NaN values are
+    ignored.
+
+    Should not be instantiated directly, use `MetricMap` or its subclasses instead."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         exploded = self.metric_data.explode(self.metric_name)
         self.map_data = exploded.groupby(self.coords_cols).agg(self.agg)[self.metric_name]
 
     def _plot_map(self, ax, is_lower_better, **kwargs):
+        """Display map data as a scatter plot."""
         sort_key = None
         if is_lower_better is None:
             is_lower_better = True
@@ -137,6 +213,15 @@ class ScatterMap(BaseMetricMap):
         return ax.scatter(coords_x, coords_y, c=map_data, **kwargs)
 
     def get_worst_coords(self, is_lower_better=None):
+        """Get coordinates with the worst metric value dependind on `is_lower_better`. If not given, `is_lower_better`
+        attribute of `self.metric` is used.
+
+        Three options are possible:
+        1. If `is_lower_better` is `True`, coordinates with maximum metric value are returned,
+        2. If `is_lower_better` is `False`, coordinates with minimum metric value are returned,
+        3. Otherwise, coordinates, whose value has maximum absolute deviation from the aggregated metric value is
+           returned.
+        """
         is_lower_better = self.is_lower_better if is_lower_better is None else is_lower_better
         if is_lower_better is None:
             data = (self.map_data - self.metric_data[self.metric_name].agg(self.agg)).abs()
@@ -148,13 +233,15 @@ class ScatterMap(BaseMetricMap):
 
 
 class BinarizedMap(BaseMetricMap):
-    """
-    The map is constructed in the following way:
-        1. All stored coordinates are divided into bins of the specified `bin_size`.
-        2. All metric values are grouped by their bin.
-        3. An aggregation is performed by calling `agg_func` for values in each bin. If no metric values were assigned
-           to a bin, `np.nan` is returned.
-        As a result, each value of the constructed map represents an aggregated metric for a particular bin.
+    """Construct a binarized metric map.
+
+    Binarization is performed in the following way:
+    1. All stored coordinates are divided into bins of the given `bin_size`,
+    2. All metric values are grouped by their bin,
+    3. An aggregation is performed by calling `agg` for values in each bin. NaN values are ignored. If no metric values
+       were assigned to a bin, `np.nan` is returned.
+
+    Should not be instantiated directly, use `MetricMap` or its subclasses instead.
     """
     def __init__(self, *args, bin_size, **kwargs):
         super().__init__(*args, **kwargs)
@@ -191,22 +278,35 @@ class BinarizedMap(BaseMetricMap):
 
     @property
     def plot_title(self):
+        """str: title of the map plot."""
         return super().plot_title + f" in {self.bin_size[0]}x{self.bin_size[1]} bins"
 
     @property
     def x_tick_labels(self):
+        """array-like: labels of x axis ticks."""
         return self.x_bin_coords
 
     @property
     def y_tick_labels(self):
+        """array-like: labels of y axis ticks."""
         return self.y_bin_coords
 
     def _plot_map(self, ax, is_lower_better, **kwargs):
+        """Display map data as an image."""
         _ = is_lower_better
         kwargs = {"interpolation": "none", "origin": "lower", "aspect": "auto", **kwargs}
         return ax.imshow(self.map_data.T, **kwargs)
 
     def get_worst_coords(self, is_lower_better=None):
+        """Get coordinates with the worst metric value dependind on `is_lower_better`. If not given, `is_lower_better`
+        attribute of `self.metric` is used.
+
+        Three options are possible:
+        1. If `is_lower_better` is `True`, coordinates with maximum metric value are returned,
+        2. If `is_lower_better` is `False`, coordinates with minimum metric value are returned,
+        3. Otherwise, coordinates, whose value has maximum absolute deviation from the aggregated metric value is
+           returned.
+        """
         is_lower_better = self.is_lower_better if is_lower_better is None else is_lower_better
         if is_lower_better is None:
             data = np.abs(self.map_data - self.metric_data[self.metric_name].agg(self.agg))
@@ -217,6 +317,19 @@ class BinarizedMap(BaseMetricMap):
         return np.unravel_index(np.nanargmax(data), self.map_data.shape)
 
     def get_bin_contents(self, coords):
+        """Get contents of a bin by its coords.
+
+        Parameters
+        ----------
+        coords : tuple with 2 elements
+            Bin index along X and Y axes.
+
+        Returns
+        -------
+        contents : None or pandas.Series
+            If no such bin exist `None` is returned. Otherwise a series with metric data in the bin is returned. Its
+            index stores metric coordinates and values - corresponding metric values.
+        """
         if coords not in self.bin_to_coords.groups:
             return None
         contents = self.bin_to_coords.get_group(coords).set_index(self.coords_cols)[self.metric_name]
@@ -224,6 +337,8 @@ class BinarizedMap(BaseMetricMap):
 
 
 class MetricMapMeta(type):
+    """A metric map metaclass that instantiates either a `scatter_map_class` or a `binarized_map_class` depending on
+    whether `bin_size` was given."""
     def __call__(cls, *args, bin_size=None, **kwargs):
         if bin_size is None:
             map_class = cls.scatter_map_class
@@ -241,6 +356,47 @@ class MetricMapMeta(type):
 
 
 class MetricMap(metaclass=MetricMapMeta):
+    """Construct a map from metric values and their coordinates.
+
+    Parameters
+    ----------
+    coords : 2d array-like with 2 columns
+        Metric coordinates for X and Y axes.
+    metric_values : 1d array-like or array of 1d arrays
+        One or more metric values for each pair of coordinates in `coords`. Must match `coords` in length.
+    coords_cols : array-like with 2 elements, optional
+        Names of X and Y coordinates. Usually names of survey headers used to extract coordinates from. Defaults to
+        ("X", "Y") if not given and cannot be inferred from `coords`.
+    metric : Metric or subclass of Metric, optional, defaults to Metric
+        The metric whose values are used to construct the map.
+    metric_name : str, optional
+        Metric name. Defaults to "metric" if not given and cannot be inferred from `metric` and `metric_values`.
+    agg : str or callable, optional
+        A function used for aggregating the map. If not given, will be determined by the value of `is_lower_better`
+        attribute of the metric class in order to highlight outliers. Passed directly to
+        `pandas.core.groupby.DataFrameGroupBy.agg`.
+    bin_size : int, float or array-like with length 2, optional
+        Bin size for X and Y axes. If single `int` or `float`, the same bin size will be used for both axes.
+
+    Attributes
+    ----------
+    metric_data : pandas.DataFrame
+        A `DataFrame` with coordinates and metric values. NaN metric values are dropped.
+    coords_cols : array-like with 2 elements
+        Names of X and Y coordinates.
+    metric_name : str
+        Name of the metric.
+    metric : Metric or subclass of Metric
+        A metric class or instance with `metric_name` and `coords_cols` attributes set.
+    agg : str or callable
+        A function used for aggregating the map.
+    bin_size : 1d np.ndarray with 2 elements
+        Bin size for X and Y axes. Available only if the map was binarized.
+    map_data : pandas.Series or 2d np.ndarray
+        If the map was not binarized, a `pandas.Series` whose index stores metric coordinates and values -
+        corresponding metric values.
+        If the map was binarized, a 2d `np.ndarray` with values for each bin along X and Y axes.
+    """
     scatter_map_class = ScatterMap
     binarized_map_class = BinarizedMap
     interactive_scatter_map_class = ScatterMapPlot
