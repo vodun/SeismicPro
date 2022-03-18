f2e3570475bc5b731b7eda8aaad65a0d21754b57
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2022-03-18 18:12:38 +0300

    Add plot geometry docs

--- seismicpro/src/survey/plot_geometry.py
+++ seismicpro/src/survey/plot_geometry.py
@@ -1,5 +1,8 @@
+"""Implements a class for interactive plotting of survey geometry"""
+
 from functools import partial
 
+import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
@@ -7,7 +10,15 @@ from ..utils import set_ticks, set_text_formatting, get_text_formatting_kwargs
 from ..utils.interactive_plot_utils import InteractivePlot, PairedPlot
 
 
-class SurveyGeometryPlot(PairedPlot):
+class SurveyGeometryPlot(PairedPlot):  # pylint: disable=too-many-instance-attributes
+    """Interactive survey geometry plot.
+
+    The plot provides 2 views:
+    * Shot view: displays shot locations. Highlights all activated receivers on click and displays the corresponding
+      common shot gather.
+    * Receiver view: displays receiver locations. Highlights all shots that activated the receiver on click and
+      displays the corresponding common receiver gather.
+    """
     def __init__(self, survey, keep_aspect=False, sort_by=None, gather_plot_kwargs=None, x_ticker=None, y_ticker=None,
                  figsize=(4.5, 4.5), fontsize=8, orientation="horizontal", **kwargs):
         kwargs = {"fontsize": fontsize, **kwargs}
@@ -39,10 +50,12 @@ class SurveyGeometryPlot(PairedPlot):
         super().__init__(orientation=orientation)
 
     def construct_main_plot(self):
+        """Construct an interactive plot of shot and receiver locations."""
         return InteractivePlot(plot_fn=[self.plot_map, self.plot_map], click_fn=self.click, unclick_fn=self.unclick,
                                title=["Shot map", "Receiver map"])
 
     def construct_aux_plot(self):
+        """Construct an interactive gather plot."""
         toolbar_position = "right" if self.orientation == "horizontal" else "left"
         aux_plot = InteractivePlot(figsize=self.figsize, toolbar_position=toolbar_position)
         aux_plot.box.layout.visibility = "hidden"
@@ -50,6 +63,7 @@ class SurveyGeometryPlot(PairedPlot):
 
     @staticmethod
     def _process_survey(survey, coord_cols):
+        # pylint: disable=import-outside-toplevel
         from ..index import SeismicIndex  # Avoid cyclic imports, remove when Survey.get_gather is optimized
         index = SeismicIndex(surveys=survey.reindex(coord_cols))
         coords = index.indices.to_frame().values[:, 1:]
@@ -61,61 +75,77 @@ class SurveyGeometryPlot(PairedPlot):
         min_coord = min(source_coords.min(), group_coords.min())
         max_coord = max(source_coords.max(), group_coords.max())
         margin = 0.05 * (max_coord - min_coord)
+        if np.isclose(margin, 0):
+            margin = 0.05 * max_coord
         return [min_coord - margin, max_coord + margin]
 
     @property
     def is_shot_view(self):
+        """bool: whether the current view displays shot locations."""
         return self.main.current_view == 0
 
     @property
     def index(self):
+        """SeismicIndex: an index to get gathers from, depends on the current view."""
         return self.source_ix if self.is_shot_view else self.group_ix
 
     @property
     def coord_x(self):
+        """np.ndarray: x coordinates of shots or receivers, depend on the current view."""
         return self.source_x if self.is_shot_view else self.group_x
 
     @property
     def coord_y(self):
+        """np.ndarray: y coordinates of shots or receivers, depend on the current view."""
         return self.source_y if self.is_shot_view else self.group_y
 
     @property
     def coords_neighbors(self):
+        """sklearn.neighbors.NearestNeighbors: nearest neighbors of shots or receivers, depend on the current view."""
         return self.source_neighbors if self.is_shot_view else self.group_neighbors
 
     @property
     def affected_coords_cols(self):
+        """list of 2 str: coordinates columns, describing highlighted objects, depend on the current view."""
         return ["GroupX", "GroupY"] if self.is_shot_view else ["SourceX", "SourceY"]
 
     @property
     def main_color(self):
+        """str: color of the plotted objects, depends on the current view."""
         return "tab:red" if self.is_shot_view else "tab:blue"
 
     @property
     def main_marker(self):
+        """str: marker of the plotted objects, depends on the current view."""
         return "*" if self.is_shot_view else "v"
 
     @property
     def aux_color(self):
+        """str: color of the highlighted objects, depends on the current view."""
         return "tab:blue" if self.is_shot_view else "tab:red"
 
     @property
     def aux_marker(self):
+        """str: marker of the highlighted objects, depends on the current view."""
         return "v" if self.is_shot_view else "*"
 
     @property
     def map_x_label(self):
+        """str: label of the x map axis, depends on the current view."""
         return "SourceX" if self.is_shot_view else "GroupX"
 
     @property
     def map_y_label(self):
+        """str: label of the y map axis, depends on the current view."""
         return "SourceY" if self.is_shot_view else "GroupY"
 
     @property
     def gather_title(self):
+        """str: map plot title, depends on the current view."""
         return "Common shot gather at " if self.is_shot_view else "Common receiver gather at "
 
     def _plot_map(self, ax, keep_aspect, x_lim, y_lim, x_ticker, y_ticker, **kwargs):
+        """Plot shot or receiver locations depending on the current view."""
         self.aux.clear()
         self.aux.box.layout.visibility = "hidden"
 
@@ -129,6 +159,7 @@ class SurveyGeometryPlot(PairedPlot):
         set_ticks(ax, "y", self.map_y_label, **y_ticker)
 
     def click(self, coords):
+        """Highlight affected shot or receiver locations and display a gather."""
         closest_ix = self.coords_neighbors.kneighbors([coords], return_distance=False).item()
         x = self.coord_x[closest_ix]
         y = self.coord_y[closest_ix]
@@ -152,6 +183,7 @@ class SurveyGeometryPlot(PairedPlot):
         return x, y
 
     def unclick(self):
+        """Remove highlighted shot or receiver locations and hide the gather plot."""
         if self.affected_scatter is not None:
             self.affected_scatter.remove()
             self.affected_scatter = None
