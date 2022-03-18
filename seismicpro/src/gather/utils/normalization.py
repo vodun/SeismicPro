Fix gather normalization

seismicpro/src/gather/utils/normalization.py
@@ -86,7 +86,7 @@ def scale_maxabs(data, min_value, max_value, clip, eps):
     max_abs += eps
     # Use np.atleast_2d(array).T to make the array 2-dimensional by adding dummy trailing axes
     # for further broadcasting to work tracewise
-    data /= np.atleast_2d(max_abs).T
+    data /= np.atleast_2d(np.asarray(max_abs)).T
     if clip:
         data = clip_inplace(data, np.float32(-1), np.float32(1))
     return data
@@ -121,7 +121,8 @@ def scale_minmax(data, min_value, max_value, clip, eps):
     # Use np.atleast_2d(array).T to make the array 2-dimensional by adding dummy trailing axes
     # for further broadcasting to work tracewise
     min_value = np.atleast_2d(np.asarray(min_value)).T
-    max_value = np.atleast_2d(np.asarray(max_value) + eps).T
+    max_value = np.atleast_2d(np.asarray(max_value)).T
+    max_value += eps
     data -= min_value
     data /= max_value - min_value
     if clip: