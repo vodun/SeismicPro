dfb828e7b4a34bffc8ba2481aad9309cc958e92a
Alexandr Kuvaev <a.kuvaev@analysiscenter.org>
2022-03-29 19:05:07 +0300

    Fix fbp loading

--- seismicpro/src/survey/survey.py
+++ seismicpro/src/survey/survey.py
@@ -655,11 +655,12 @@ class Survey:  # pylint: disable=too-many-instance-attributes
                                       decimal=decimal, encoding=encoding, **kwargs)
 
         headers = self.headers
+        headers_index = self.headers.index.names
         headers.reset_index(inplace=True)
         headers = headers.merge(first_breaks_df, on=trace_id_cols)
         if headers.empty:
             raise ValueError('Empty headers after first breaks loading.')
-        headers.set_index(self.headers.index.names, inplace=True)
+        headers.set_index(headers_index, inplace=True)
         headers.sort_index(kind="stable", inplace=True)
         self.headers = headers
         return self
