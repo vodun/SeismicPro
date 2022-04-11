c33a93b6a84807b1eb512a1fd472d5a58b899bd8
alexanderkuvaev <alexandrkuvaev@yandex.ru>
2022-04-11 15:32:11 +0300

    indexed_by now returnes list instead of tuple

--- seismicpro/src/gather/gather.py
+++ seismicpro/src/gather/gather.py
@@ -81,7 +81,7 @@ class Gather:
     @property
     def indexed_by(self):
         """str or tuple of str: Names of header indices."""
-        index_names = tuple(self.headers.index.names)
+        index_names = list(self.headers.index.names)
         if len(index_names) == 1:
             return index_names[0]
         return index_names
