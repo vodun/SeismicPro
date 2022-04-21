eaefb9c7d1cbc8631f9a6c9d187f1a17988dc1b8
Dmitry Podvyaznikov <podviaznikoff@gmail.com>
2022-04-21 13:04:21 +0000

    Update benchmark

--- seismicpro/benchmark/utils.py
+++ seismicpro/benchmark/utils.py
@@ -1,5 +1,6 @@
 """ SeismicPro-specific functions for Benchmark """
 
+import os
 import sys
 sys.path.append('../..')
 
@@ -10,9 +11,12 @@ def make_benchmark_data(path):
     """ TODO """
     # The geometry defined below should be changed only together with survey filtering parameters
     # to ensure that after filtering all the gathers \ supergathers have the same number of traces
-    make_prestack_segy(path, survey_size=(400, 400), sources_step=(5, 5), receivers_step=(5, 5),
-                       activation_dist=(50, 50), bin_size=(10, 10))
+    if not os.path.isfile(path):
+        make_prestack_segy(path, survey_size=(400, 400), sources_step=(5, 5), receivers_step=(5, 5),
+                           activation_dist=(50, 50), bin_size=(10, 10))
 
+def load_benchmark_data(path):
+    """ TODO """
     # Load required headers and add synthetic FirstBreak times
     sur = Survey(path, header_index=['INLINE_3D', 'CROSSLINE_3D'],
                  header_cols='offset', name='raw')
