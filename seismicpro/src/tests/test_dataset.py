d13a679e35d5fddf890c1ab619744df24347184b
alexanderkuvaev <alexandrkuvaev@yandex.ru>
2022-04-21 14:20:15 +0300

    Fix dataset tests

--- seismicpro/src/tests/test_dataset.py
+++ seismicpro/src/tests/test_dataset.py
@@ -12,19 +12,19 @@ def survey(segy_path):
 
 def test_dataset_creation_from_survey(survey):
     """test_index_creation_from_survey"""
-    _ = SeismicDataset(surveys=survey)
+    _ = SeismicDataset(survey)
 
 def test_dataset_creation_from_index(survey):
     """test_dataset_creation_from_index"""
-    index = SeismicIndex(surveys=survey)
-    _ = SeismicDataset(index=index)
+    index = SeismicIndex(survey)
+    _ = SeismicDataset(index)
 
 def test_dataset_split(survey):
     """test_dataset_split"""
-    dataset = SeismicDataset(surveys=survey)
+    dataset = SeismicDataset(survey)
     dataset.split()
 
 def test_dataset_collect_stats(survey):
     """test_dataset_collect_stats"""
-    dataset = SeismicDataset(surveys=survey)
+    dataset = SeismicDataset(survey)
     dataset.collect_stats()
