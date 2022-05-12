ddb8f12e62b872a650b38219fe35105e0c5ffd65
Sobolevskiy Vadim <werever@mail.ru>
2022-05-12 15:25:08 +0000

    naming and docs fixes

--- seismicpro/src/weathering_velocity/metrics.py
+++ seismicpro/src/weathering_velocity/metrics.py
@@ -14,44 +14,45 @@ class WeatheringVelocityMetric(PipelineMetric):
     vmin = 0
     vmax = 0.05
     is_lower_better = True
-    views = ("plot", "plot_wv")
+    views = ("plot_gather", "plot_weathering_velocity")
     args_to_unpack = ("gather", "weathering_velocity")
 
     @staticmethod
     def calc(gather, weathering_velocity, first_breaks_col=HDR_FIRST_BREAK, threshold_times=50, **kwargs):
         """Calculates the weathering metric value.
 
-        Weathering metric calculated as fraction of first breaking times that stands out from a weathering velocity
-        curve (piecewise linear function) more that `threshold_times` relative to the total number of first breaking
-        times.
+        Weathering metric calculated as a fraction of traces in the gather whose first break times differ
+        from estimated by weathering velocity model for more than `threshold_times`.
 
         Parameters
         ----------
         weathering_velocity : WeatheringVelocity
-            Calculated WeatheringVelocity. Use `calculate_weathering_velocity` to calculate it.
+            Weathering velocity used to estimate expected first break times.
         first_breaks_col : str, defaults to HDR_FIRST_BREAK
-            Column name  from `self.headers` where first breaking times are stored.
-        threshold_times: int or float, defaults to 50
+            Column name  from `gather.headers` where first breaking times are stored.
+        threshold_times: float, defaults to 50
             Threshold for the weathering metric calculation.
 
         Returns
         -------
         metric : float
-            Fraction of the first breaks stands out from the weathering velocity curve more than given threshold time.
+            Fraction of traces in the gather whose first break times differ from estimated by weathering velocity model
+            for more than threshold_times.
         """
         _ = kwargs
         metric = np.abs(weathering_velocity(gather.offsets) - gather[first_breaks_col].ravel()) > threshold_times
         return np.mean(metric)
 
     @pass_calc_args
-    def plot(cls, gather, weathering_velocity, first_breaks_col=HDR_FIRST_BREAK, threshold_times=50, **kwargs):
+    def plot_gather(cls, gather, weathering_velocity, first_breaks_col=HDR_FIRST_BREAK, threshold_times=50, **kwargs):
         """Plot the gather and the first break points."""
         _ = weathering_velocity, threshold_times
         event_headers = kwargs.pop('event_headers', {'headers': first_breaks_col})
         gather.plot(event_headers=event_headers, **kwargs)
 
     @pass_calc_args
-    def plot_wv(cls, gather, weathering_velocity, first_breaks_col=HDR_FIRST_BREAK, threshold_times=50, **kwargs):
+    def plot_weathering_velocity(cls, gather, weathering_velocity, first_breaks_col=HDR_FIRST_BREAK,
+                                 threshold_times=50, **kwargs):
         """Plot the first break picking points, weathering velocity curve, thresholds, and weathering model
         parameters."""
         _ = gather, first_breaks_col
