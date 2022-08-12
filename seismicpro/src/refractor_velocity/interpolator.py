8d7a2be0b2684336b24f223f1da545dbf3178ce7
anton-br <ziliboba753@gmail.com>
2022-08-12 14:16:43 +0300

    rewrite interpolator

--- seismicpro/src/refractor_velocity/interpolator.py
+++ seismicpro/src/refractor_velocity/interpolator.py
@@ -3,25 +3,31 @@
 import numpy as np
 from tqdm.auto import tqdm
 
-from ..utils import CloughTocherInterpolator, Coordinates
-from .refractor_velocity_field import RefractorVelocityField
+from ..utils import CloughTocherInterpolator, Coordinates, get_cols
+from .refractor_velocity_field import RefractorVelocityField, RefractorVelocity
 
 
 class RefractorVelocityInterpolator():
-    def __init__(self, refractor_velocity):
+    def __init__(self, refractor_velocity, smoothing_radius=None):
         self.field = RefractorVelocityField()
         self.refractor_velocity = refractor_velocity / 1000 # Convert m/sec to m/ms
+        self.smoothing_radius = smoothing_radius
 
-    def from_supergathers(self, supergather_survey, first_breaks_col, wv_kwargs=None):
-        """Interpolate wv using supergathers"""
+    def from_supergathers(self, supergather_survey, first_breaks_col, rv_kwargs=None):
+        """Interpolate rv using supergathers"""
         # TODO: Add option to set refractor velocity as Uphole_depth / Uphole_time
-        wv_kwargs = dict() if wv_kwargs is None else wv_kwargs
-        wv_kwargs = {"n_refractors": 2, "init": {"t0": 0}, **wv_kwargs}
-        for ix in tqdm(supergather_survey.headers.index.unique()):
-            g = supergather_survey.get_gather(ix)
-            rv = g.calculate_refractor_velocity(first_breaks_col=first_breaks_col, **wv_kwargs)
-            rv.coords = Coordinates(coords=g.get_central_gather()[["CDP_X", "CDP_Y"]][0], names=["CDP_X", "CDP_Y"])
+        rv_kwargs = dict() if rv_kwargs is None else rv_kwargs
+        rv_kwargs = {"n_refractors": 2, "init": {"t0": 0}, **rv_kwargs}
+        grouped_headers = supergather_survey.headers.groupby(["SUPERGATHER_INLINE_3D", "SUPERGATHER_CROSSLINE_3D"])
+        for _, sub_headers in tqdm(grouped_headers):
+            rv = RefractorVelocity().from_first_breaks(offsets=sub_headers["offset"].values,
+                                                      fb_times=sub_headers[first_breaks_col].values, **rv_kwargs)
+            sp_coords = get_cols(sub_headers, ["SUPERGATHER_INLINE_3D", "SUPERGATHER_CROSSLINE_3D"])
+            mask = np.all(sub_headers[["INLINE_3D", "CROSSLINE_3D"]].values == sp_coords, axis=1)
+            rv.coords = Coordinates(coords=sub_headers[mask][["CDP_X", "CDP_Y"]].values[0], names=["CDP_X", "CDP_Y"])
             self.field.update(rv)
+        if self.smoothing_radius is not None:
+            self.field = self.field.smooth(self.smoothing_radius)
         self.field.create_interpolator('ct')
         return self
 
@@ -35,7 +41,7 @@ class RefractorVelocityInterpolator():
         v1 = values[:, -n_refractors] / 1000
         first_crvrs = np.array([calculate_crossovers(self.refractor_velocity, 0, v1, t0)]).reshape(-1, 1)
         refractor_velocity = np.zeros((len(coords), 1)) + self.refractor_velocity
-        res = np.hstack((first_crvrs, values[:, 1: -n_refractors], refractor_velocity, values[:, -n_refractors:]))
+        res = np.hstack((first_crvrs, values[:, 1: -n_refractors], refractor_velocity, values[:, -n_refractors:]/1000))
         if is_1d_coords:
             return res[0]
         return res
