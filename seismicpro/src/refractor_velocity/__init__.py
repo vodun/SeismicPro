f2da05e09faf4fe0bd8ff0c344d90e8acf60eda3
alexanderkuvaev <alexandrkuvaev@yandex.ru>
2022-08-25 19:24:20 +0300

    Minor doc fix

--- seismicpro/src/refractor_velocity/__init__.py
+++ seismicpro/src/refractor_velocity/__init__.py
@@ -1,4 +1,4 @@
-"""Classes for the velocity models of an upper part of the section"""
+"""Classes to construct a velocity model of an upper part of the section"""
 
 from .refractor_velocity import RefractorVelocity
 from .refractor_velocity_field import RefractorVelocityField
