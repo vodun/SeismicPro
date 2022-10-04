407857bb0e6a2d97bd21fe8afd9adfcea25ff3bc
alexanderkuvaev <alexandrkuvaev@yandex.ru>
2022-10-04 15:22:13 +0300

    Simplify postprocess_params

--- seismicpro/src/refractor_velocity/utils.py
+++ seismicpro/src/refractor_velocity/utils.py
@@ -16,22 +16,14 @@ def postprocess_params(params):
     - Velocities of refractors are non-negative and increasing.
     """
     is_1d = (params.ndim == 1)
-    params = np.atleast_2d(params).copy()
+
+    # Ensure that all params are non-negative
+    params = np.clip(np.atleast_2d(params), 0, None)
+
+    # Ensure that velocities of refractors and crossover offsets are non-decreasing
     n_refractors = params.shape[1] // 2
-
-    # Ensure that t0 is non-negative
-    np.clip(params[:, 0], 0, None, out=params[:, 0])
-
-    # Ensure that velocities of refractors are non-negative and increasing
-    velocities = params[:, n_refractors:]
-    np.clip(velocities[:, 0], 0, None, out=velocities[:, 0])
-    np.maximum.accumulate(velocities, axis=1, out=velocities)
-
-    # Ensure that crossover offsets are non-negative and increasing
-    if n_refractors > 1:
-        cross_offsets = params[:, 1:n_refractors]
-        np.clip(cross_offsets[:, 0], 0, None, out=cross_offsets[:, 0])
-        np.maximum.accumulate(cross_offsets, axis=1, out=cross_offsets)
+    np.maximum.accumulate(params[:, n_refractors:], axis=1, out=params[:, n_refractors:])
+    np.maximum.accumulate(params[:, 1:n_refractors], axis=1, out=params[:, 1:n_refractors])
 
     if is_1d:
         return params[0]
