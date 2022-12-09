ad2b8c0390c30f847735d12056e4073cb5ad5782
anton-br <ziliboba753@gmail.com>
2022-12-09 11:40:36 +0300

    add std plot

--- seismicpro/avo/avo.py
+++ seismicpro/avo/avo.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 
@@ -10,7 +11,7 @@ from ..utils import to_list
 
 # """TODO: Save values, split them by offset based on given bins. be able to regroup them by any given index (coords)"""
 class AmplitudeOffsetDistribution:
-    def __init__(self, survey, avo_column, bin_size, indexed_by=None):
+    def __init__(self, survey, avo_column, bin_size, indexed_by=None, name=None):
         self.headers = survey.headers.reset_index().copy(deep=False)
         if "offset" not in self.headers:
             raise ValueError("Missing offset header")
@@ -22,6 +23,7 @@ class AmplitudeOffsetDistribution:
         self.data = self.headers[[self.avo_column, "offset"]].to_numpy()
         self.indexed_by = indexed_by or survey.indexed_by
 
+        self.name = name
         self.bin_bounds = None
         self.agg_stats = None
         self.combine_to_gathers(bin_size=bin_size)
@@ -51,29 +53,57 @@ class AmplitudeOffsetDistribution:
             self.indexed_by = indexed_by
 
         avo_stats, offsets = self.headers[[self.avo_column, "offset"]].to_numpy().T
-        self.agg_stats = aggregate_by_bins_numba(avo_stats, offsets, self.bin_bounds, self.gather_bounds)
+        agg_stats = aggregate_by_bins_numba(avo_stats, offsets, self.bin_bounds, self.gather_bounds)
+        agg_stats[agg_stats == 0] = np.nan
+        self.agg_stats = agg_stats
 
     def plot(self, title=None, figsize=(12, 7), dot_size=3, avg_size=8, dpi=100, save_to=None):
         indices = np.tile(self.bin_bounds, self.agg_stats.shape[0])
 
         bins_mean = np.nanmean(self.agg_stats, axis=0)
-        plt.figure(figsize=figsize)
-        plt.plot(indices, self.agg_stats.ravel(), '.', markersize=3)
-        plt.plot(self.bin_bounds, bins_mean, 'v', color='r', markersize=8)
+        fig = plt.figure(figsize=figsize)
+        plt.plot(indices, self.agg_stats.ravel(), 'o', markersize=dot_size)
+        plt.plot(self.bin_bounds, bins_mean, 'v', color='r', markersize=avg_size)
 
-        plt.ylabel('Amplitude')
-        plt.xlabel('Offset')
-        if title:
-            plt.title(title)
         plt.grid()
+        plt.xlabel('Offset')
+        plt.ylabel('Amplitude')
+        plt.title(title)
 
         if save_to is not None:
-            plt.savefig(save_to, dpi=dpi)
+            save_figure(fig, save_to, dpi=dpi)
         plt.show()
 
-    # def plot_std(self, *args):
+    def plot_std(self, *args, title=None, figsize=(12, 7), save_to=None):
+        if self.name is None:
+            raise ValueError("self.name must be specified")
 
+        data = self.agg_stats.ravel()
+        indices = np.tile(self.bin_bounds, self.agg_stats.shape[0])
+        names = [self.name] * len(indices)
+        if args:
+            for ix, other in enumerate(args):
+                if other.name is None:
+                    raise ValueError("`name` attribute must be specified for all AVOs")
+                if np.any(self.bin_bounds != other.bin_bounds):
+                    raise ValueError("All AVOs must have same bin_size")
+                if np.any(self.indexed_by != other.indexed_by):
+                    raise ValueError("All AVOs must be indexed by the same header")
 
+                other_data =other.agg_stats.ravel()
+                data = np.append(data, other_data)
+                indices = np.append(indices, indices)
+                names += [other.name] * len(other_data)
+
+        sns.set_style("darkgrid")
+        fig = plt.figure(figsize=figsize)
+        sns.lineplot(x=indices, y=data, hue=names, ci="sd")
+        plt.xlabel('Offset')
+        plt.ylabel('Amplitude')
+        plt.title(title)
+        if save_to is not None:
+            save_figure(fig, save_to, dpi=dpi)
+        plt.show()
 
 
 
