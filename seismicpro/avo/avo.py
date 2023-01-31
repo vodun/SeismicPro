cfc5b9037ab205b10bd5682cf7ef3a2313353fa5
anton-br <ziliboba753@gmail.com>
2023-01-31 12:53:04 +0300

    rewrite metrics and change plots a little

--- seismicpro/avo/avo.py
+++ seismicpro/avo/avo.py
@@ -1,4 +1,4 @@
-import os
+"""AVO"""
 
 import numpy as np
 import seaborn as sns
@@ -10,11 +10,11 @@ from ..utils import to_list, save_figure
 
 
 class AmplitudeOffsetDistribution:
-    def __init__(self, headers, indexed_by, bins_bounds, agg_stats, name):
+    def __init__(self, headers, agg_stats, indexed_by, bins_bounds, name):
         self.headers = headers
+        self.agg_stats = agg_stats
         self.indexed_by = indexed_by
         self.bins_bounds = bins_bounds
-        self.agg_stats = agg_stats
         self.name = name
         self.data = None
         self.gather_bounds = None
@@ -23,8 +23,7 @@ class AmplitudeOffsetDistribution:
         self.bins_approx = None
 
         # Metrics
-        self.std = None
-        self.corr = None
+        self.metrics = {}
 
     @classmethod
     def from_survey(cls, survey, avo_column, bin_size, indexed_by=None, name=None):
@@ -47,7 +46,7 @@ class AmplitudeOffsetDistribution:
         bins_bounds = cls._get_bounds_by_bin_size(bin_size, headers["offset"].max())
         agg_stats = cls._compute_in_bins(data=data, bins_bounds=bins_bounds, gather_bounds=gather_bounds)
 
-        self = cls(headers, indexed_by, bins_bounds, agg_stats, name)
+        self = cls(headers, agg_stats, indexed_by, bins_bounds, name)
         self.data = data
         self.gather_bounds = gather_bounds
         return self
@@ -61,6 +60,7 @@ class AmplitudeOffsetDistribution:
         data = self.data
         bins_bounds = self.bins_bounds
         gather_bounds = self.gather_bounds
+        name = name if name is not None else self.name
 
         if bin_size is not None:
             bins_bounds = self._get_bounds_by_bin_size(bin_size, self.headers["offset"].max())
@@ -70,7 +70,6 @@ class AmplitudeOffsetDistribution:
             data = data[groups_ix]
 
         agg_stats = self._compute_in_bins(data=data, bins_bounds=bins_bounds, gather_bounds=gather_bounds)
-        name = name if name is not None else self.name
 
         new_self = type(self)(self.headers.copy(deep=False), indexed_by, bins_bounds, agg_stats, name)
         new_self.data = data
@@ -82,10 +81,15 @@ class AmplitudeOffsetDistribution:
         new_index = to_list(new_index)
         headers_grouper = headers.groupby(new_index).grouper
         groups_ix = headers_grouper.result_ilocs()
-        gathers_bounds = np.cumsum([0, *headers_grouper.size().to_numpy()])
-
-        gather_bounds = np.array(tuple(zip(gathers_bounds[:-1], gathers_bounds[1:]-1)))
+        gather_bounds = np.cumsum([0, *headers_grouper.size().to_numpy()])
+        gather_bounds = np.array(tuple(zip(gather_bounds[:-1], gather_bounds[1:]-1)))
         return gather_bounds, groups_ix
+
+    @staticmethod
+    def _get_bounds_by_bin_size(bin_size, max_offset):
+        if isinstance(bin_size, (int, np.integer)):
+            return np.arange(0, max_offset+bin_size, bin_size)
+        return np.cumsum([0, *bin_size])
 
     @staticmethod
     def _compute_in_bins(data, bins_bounds, gather_bounds):
@@ -93,12 +97,6 @@ class AmplitudeOffsetDistribution:
         agg_stats = aggregate_by_bins_numba(avo_stats, offsets, bins_bounds, gather_bounds)
         agg_stats[agg_stats == 0] = np.nan
         return agg_stats
-
-    @staticmethod
-    def _get_bounds_by_bin_size(bin_size, max_offset):
-        if isinstance(bin_size, (int, np.integer)):
-            return np.arange(0, max_offset+bin_size, bin_size)
-        return np.cumsum([0, *bin_size])
 
     def qc(self, names, pol_degree=3):
 
@@ -111,12 +109,11 @@ class AmplitudeOffsetDistribution:
             metric_func = metrics_dict.get(name)
             if metric_func is None:
                 raise ValueError("")
+            # TODO: figure out how to pass kwargs to metric func normally
             kwargs = {"pol_degree" : pol_degree} if name == "corr" else {}
-            result = metric_func(**kwargs)
-            setattr(self, name, result)
+            self.metrics[name] = metric_func(**kwargs)
 
     def _calculate_corr(self, pol_degree):
-        # TODO: seems like we have to calcualte correlations in the range of offsets, not bins.
         not_nans = ~np.isnan(self.bins_mean)
         bounds = self.bins_bounds[:-1][not_nans]
         means = self.bins_mean[not_nans]
@@ -124,23 +121,21 @@ class AmplitudeOffsetDistribution:
         self.bins_approx = np.polyval(poly, self.bins_bounds[:-1])
         return np.corrcoef(means, np.polyval(poly, bounds))[0][1]
 
-    def plot(self, show_poly=False, title=None, figsize=(12, 7), dot_size=2, avg_size=8, dpi=100, save_to=None):
-        indices = np.tile(self.bins_bounds[:-1], self.agg_stats.shape[0])
-
+    def plot(self, show_qc=False, show_poly=False, title=None, figsize=(12, 7), dot_size=2, avg_size=8, dpi=100,
+             save_to=None):
         fig, ax = plt.subplots(figsize=figsize)
+        indices = np.tile(self.bins_bounds[:-1], self.agg_stats.shape[0])
         ax.plot(indices, self.agg_stats.ravel(), 'o', markersize=dot_size)
         ax.plot(self.bins_bounds[:-1], self.bins_mean, 'v', color='r', markersize=avg_size)
-
         if show_poly:
             if self.bins_approx is None:
                 raise ValueError("Calculate QC for `corr` before using `show_poly`")
-            ax.plot(self.bins_bounds[:-1], self.bins_approx, '-', c='g')
-
+            ax.plot(self.bins_bounds[:-1], self.bins_approx, '--', c='g', zorder=3)
         ax.grid()
-        self._finalize_plot(fig, ax, title, save_to, dpi)
+        self._finalize_plot(fig, ax, show_qc, title, save_to, dpi)
 
-
-    def plot_std(self, *args, align=False, show_poly=False, title=None, figsize=(12, 7), dpi=100, save_to=None):
+    def plot_std(self, *args, align=False, title=None, figsize=(12, 7), dpi=100,
+                 save_to=None):
         if self.name is None:
             raise ValueError("self.name must be specified")
 
@@ -148,7 +143,7 @@ class AmplitudeOffsetDistribution:
         indices = np.tile(self.bins_bounds[:-1], self.agg_stats.shape[0])
         names = [self.name] * len(indices)
         if args:
-            for ix, other in enumerate(args):
+            for other in args:
                 if other.name is None:
                     raise ValueError("`name` attribute must be specified for all AVOs")
                 if np.any(self.bins_bounds != other.bins_bounds):
@@ -171,17 +166,18 @@ class AmplitudeOffsetDistribution:
         sns.set_style("darkgrid")
         fig, ax = plt.subplots(figsize=figsize)
         sns.lineplot(x=indices, y=data, hue=names, ci="sd", ax=ax)
-        if show_poly:
-            if self.bins_approx is None:
-                raise ValueError("Calculate QC for `corr` before using `show_poly`")
-            ax.plot(self.bins_bounds[:-1], self.bins_approx, '-', c='g')
 
-        self._finalize_plot(fig, ax, title, save_to, dpi)
+        self._finalize_plot(fig, ax, False, title, save_to, dpi)
         sns.set_style("ticks")
 
-    def _finalize_plot(self, fig, ax, title, save_to, dpi):
+    def _finalize_plot(self, fig, ax, show_qc, title, save_to, dpi):
         ax.set_xlabel('Offset')
         ax.set_ylabel('Amplitude')
+        if show_qc:
+            title = "" if title is None else title
+            for name, value in self.metrics.items():
+                sep = "\n" if title else ""
+                title = title + sep + f"{name} : {value:.4}"
         ax.set_title(title)
         if save_to is not None:
             save_figure(fig, save_to, dpi=dpi)
@@ -200,30 +196,3 @@ class AmplitudeOffsetDistribution:
     #     ).run(**pipeline_kwargs)
 
     #     return cls(survey[["avo_stats"]], survey["offset"], bins)
-
-# def __init__(self, survey, avo_column, bin_size, indexed_by=None, name=None):
-#         self.headers = survey.headers.reset_index()
-#         if "offset" not in self.headers:
-#             raise ValueError("Missing offset header")
-#         self.max_offset = self.headers["offset"].max()
-#         self.avo_column = avo_column
-
-#         self.data = self.headers[[self.avo_column, "offset"]].to_numpy()
-#         self.indexed_by = None
-#         if indexed_by is None:
-#             # Use index from survey by default and avoid further data reindex
-#             gather_bounds = np.where(~survey.headers.index.duplicated())[0]
-#             self.gather_bounds = np.array(tuple(zip(gather_bounds[:-1], gather_bounds[1:]-1)))
-#             self.indexed_by = survey.indexed_by
-
-#         self.name = name if name is not None else survey.name
-#         self.bins_bounds = None
-#         self.agg_stats = None
-#         self.bins_mean = None
-#         self.bins_approx = None
-
-#         # Metrics
-#         self.std = None
-#         self.corr = None
-
-#         self.regroup(bin_size=bin_size, indexed_by=indexed_by or self.indexed_by)
