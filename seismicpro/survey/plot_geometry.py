6cbac6c7bfe7f6f88dfe21d6035b92f5135c4f3d
alexanderkuvaev <alexandrkuvaev@yandex.ru>
2023-02-27 13:20:08 +0300

    Update geometry plot

--- seismicpro/survey/plot_geometry.py
+++ seismicpro/survey/plot_geometry.py
@@ -2,72 +2,78 @@
 
 from functools import partial
 
-import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
-from ..utils import calculate_axis_limits, set_ticks, set_text_formatting, get_text_formatting_kwargs
-from ..utils.interactive_plot_utils import InteractivePlot, PairedPlot
+from ..utils import get_first_defined, calculate_axis_limits
+from ..utils import InteractivePlot, PairedPlot, set_ticks, set_text_formatting, get_text_formatting_kwargs
+from ..metrics.interactive_map import NonOverlayingIndicesPlot, OverlayingIndicesPlot
 
 
 class SurveyGeometryPlot(PairedPlot):  # pylint: disable=too-many-instance-attributes
     """Interactive survey geometry plot.
 
     The plot provides 2 views:
-    * Shot view: displays shot locations. Highlights all activated receivers on click and displays the corresponding
-      common shot gather.
-    * Receiver view: displays receiver locations. Highlights all shots that activated the receiver on click and
+    * Source view: displays locations of seismic sources. Highlights all activated receivers on click and displays the
+      corresponding common source gather.
+    * Receiver view: displays locations of receivers. Highlights all sources that activated the receiver on click and
       displays the corresponding common receiver gather.
     """
 
     #pylint: disable-next=too-many-arguments
-    def __init__(self, survey, show_contour=True, keep_aspect=False, sort_by=None, gather_plot_kwargs=None,
-                 x_ticker=None, y_ticker=None, figsize=(4.5, 4.5), fontsize=8, orientation="horizontal", **kwargs):
+    def __init__(self, survey, show_contour=True, keep_aspect=False, source_id_cols=None, source_sort_by=None,
+                 receiver_id_cols=None, receiver_sort_by=None, sort_by=None, gather_plot_kwargs=None, x_ticker=None,
+                 y_ticker=None, figsize=(4.5, 4.5), fontsize=8, orientation="horizontal", **kwargs):
         kwargs = {"fontsize": fontsize, **kwargs}
         (x_ticker, y_ticker), self.scatter_kwargs = set_text_formatting(x_ticker, y_ticker, **kwargs)
         text_kwargs = get_text_formatting_kwargs(**kwargs)
         if gather_plot_kwargs is None:
             gather_plot_kwargs = {}
-        self.gather_plot_kwargs = {"title": None, **text_kwargs, **gather_plot_kwargs}
+        gather_plot_kwargs = {"title": None, **text_kwargs, **gather_plot_kwargs}
 
-        self.sort_by = sort_by
         self.figsize = figsize
         self.orientation = orientation
 
-        # Reindex the survey to speed up gather selection and fit nearest neighbors to project a click on the closest
-        # shot or receiver
-        source_params = self._process_survey(survey, ["SourceX", "SourceY"])
-        self.source_sur, self.source_x, self.source_y, self.source_neighbors = source_params
-        group_params = self._process_survey(survey, ["GroupX", "GroupY"])
-        self.group_sur, self.group_x, self.group_y, self.group_neighbors = group_params
+        # Reindex the survey by source and receiver to speed up gather selection and fit nearest neighbors to project a
+        # click on the map to the closest source or receiver
+        self.source_map = survey.construct_fold_map(by="source", id_cols=source_id_cols)
+        self.source_survey = survey.reindex(self.source_map.index_cols)
+        self.source_coords = self.source_map.map_data.index.to_frame(index=False).to_numpy()
+        self.source_neighbors = NearestNeighbors(n_neighbors=1).fit(self.source_coords)
+        self.source_sort_by = get_first_defined(source_sort_by, sort_by)
+
+        self.receiver_map = survey.construct_fold_map(by="receiver", id_cols=receiver_id_cols)
+        self.receiver_survey = survey.reindex(self.receiver_map.index_cols)
+        self.receiver_coords = self.receiver_map.map_data.index.to_frame(index=False).to_numpy()
+        self.receiver_neighbors = NearestNeighbors(n_neighbors=1).fit(self.receiver_coords)
+        self.receiver_sort_by = get_first_defined(receiver_sort_by, sort_by)
 
         # Calculate axes limits to fix them to avoid map plot shifting on view toggle
-        x_lim = calculate_axis_limits(np.concatenate([self.source_x, self.group_x]))
-        y_lim = calculate_axis_limits(np.concatenate([self.source_y, self.group_y]))
+        x_lim = calculate_axis_limits([self.source_coords[:, 0].min(), self.source_coords[:, 0].max(),
+                                       self.receiver_coords[:, 0].min(), self.receiver_coords[:, 0].max()])
+        y_lim = calculate_axis_limits([self.source_coords[:, 1].min(), self.source_coords[:, 1].max(),
+                                       self.receiver_coords[:, 1].min(), self.receiver_coords[:, 1].max()])
         contours = survey.geographic_contours if show_contour else None
         self.plot_map = partial(self._plot_map, contours=contours, keep_aspect=keep_aspect, x_lim=x_lim, y_lim=y_lim,
                                 x_ticker=x_ticker, y_ticker=y_ticker, **self.scatter_kwargs)
-        self.affected_scatter = None
+        self.plot_gather = partial(self._plot_gather, **gather_plot_kwargs)
+        self.activated_scatter = None
 
         super().__init__(orientation=orientation)
 
     def construct_main_plot(self):
-        """Construct a clickable plot of shot and receiver locations."""
+        """Construct a clickable plot of source and receiver locations."""
         return InteractivePlot(plot_fn=[self.plot_map, self.plot_map], click_fn=self.click, unclick_fn=self.unclick,
-                               title=["Shot map", "Receiver map"])
+                               title=["Map of sources", "Map of receivers"], figsize=self.figsize)
 
     def construct_aux_plot(self):
         """Construct a gather plot."""
         toolbar_position = "right" if self.orientation == "horizontal" else "left"
-        aux_plot = InteractivePlot(figsize=self.figsize, toolbar_position=toolbar_position)
+        kwargs = {"plot_fn": self.plot_gather, "figsize": self.figsize, "toolbar_position": toolbar_position}
+        has_overlays = self.source_map.has_overlaying_indices or self.receiver_map.has_overlaying_indices
+        aux_plot_type = OverlayingIndicesPlot if has_overlays else NonOverlayingIndicesPlot
+        aux_plot = aux_plot_type(**kwargs)
         aux_plot.box.layout.visibility = "hidden"
         return aux_plot
-
-    @staticmethod
-    def _process_survey(survey, coord_cols):
-        survey = survey.reindex(coord_cols)
-        coords = survey.indices.to_frame(index=False).to_numpy()
-        coords_neighbors = NearestNeighbors(n_neighbors=1).fit(coords)
-        return survey, coords[:, 0], coords[:, 1], coords_neighbors
 
     @property
     def is_shot_view(self):
@@ -75,27 +81,30 @@ class SurveyGeometryPlot(PairedPlot):  # pylint: disable=too-many-instance-attri
         return self.main.current_view == 0
 
     @property
+    def active_map(self):
+        return self.source_map if self.is_shot_view else self.receiver_map
+
+    @property
     def survey(self):
         """Survey: a survey to get gathers from, depends on the current view."""
-        return self.source_sur if self.is_shot_view else self.group_sur
+        return self.source_survey if self.is_shot_view else self.receiver_survey
 
     @property
-    def coord_x(self):
-        """np.ndarray: x coordinates of shots or receivers, depend on the current view."""
-        return self.source_x if self.is_shot_view else self.group_x
-
-    @property
-    def coord_y(self):
-        """np.ndarray: y coordinates of shots or receivers, depend on the current view."""
-        return self.source_y if self.is_shot_view else self.group_y
+    def coords(self):
+        """np.ndarray: Coordinates of shots or receivers, depend on the current view."""
+        return self.source_coords if self.is_shot_view else self.receiver_coords
 
     @property
     def coords_neighbors(self):
         """sklearn.neighbors.NearestNeighbors: nearest neighbors of shots or receivers, depend on the current view."""
-        return self.source_neighbors if self.is_shot_view else self.group_neighbors
+        return self.source_neighbors if self.is_shot_view else self.receiver_neighbors
+    
+    @property
+    def sort_by(self):
+        return self.source_sort_by if self.is_shot_view else self.receiver_sort_by
 
     @property
-    def affected_coords_cols(self):
+    def activated_coords_cols(self):
         """list of 2 str: coordinates columns, describing highlighted objects, depend on the current view."""
         return ["GroupX", "GroupY"] if self.is_shot_view else ["SourceX", "SourceY"]
 
@@ -129,17 +138,12 @@ class SurveyGeometryPlot(PairedPlot):  # pylint: disable=too-many-instance-attri
         """str: label of the y map axis, depends on the current view."""
         return "SourceY" if self.is_shot_view else "GroupY"
 
-    @property
-    def gather_title(self):
-        """str: map plot title, depends on the current view."""
-        return "Common shot gather at " if self.is_shot_view else "Common receiver gather at "
-
     def _plot_map(self, ax, contours, keep_aspect, x_lim, y_lim, x_ticker, y_ticker, **kwargs):
         """Plot shot or receiver locations depending on the current view."""
         self.aux.clear()
         self.aux.box.layout.visibility = "hidden"
 
-        ax.scatter(self.coord_x, self.coord_y, color=self.main_color, marker=self.main_marker, **kwargs)
+        ax.scatter(*self.coords.T, color=self.main_color, marker=self.main_marker, **kwargs)
         if contours is not None:
             for contour in contours:
                 ax.fill(contour[:, 0, 0], contour[:, 0, 1], facecolor="gray", edgecolor="black", alpha=0.5)
@@ -151,30 +155,35 @@ class SurveyGeometryPlot(PairedPlot):  # pylint: disable=too-many-instance-attri
         set_ticks(ax, "x", self.map_x_label, **x_ticker)
         set_ticks(ax, "y", self.map_y_label, **y_ticker)
 
-    def click(self, coords):
-        """Highlight affected shot or receiver locations and display a gather."""
-        closest_ix = self.coords_neighbors.kneighbors([coords], return_distance=False).item()
-        x = self.coord_x[closest_ix]
-        y = self.coord_y[closest_ix]
-        gather = self.survey.get_gather((x, y), copy_headers=False)
+    def _plot_gather(self, ax, coords, index, **kwargs):
+        _ = coords
+        gather = self.survey.get_gather(index)
         if self.sort_by is not None:
             gather = gather.sort(by=self.sort_by)
+        gather.plot(ax=ax, **kwargs)
+        self._plot_activated(gather[self.activated_coords_cols])
 
-        if self.affected_scatter is not None:
-            self.affected_scatter.remove()
-        self.affected_scatter = self.main.ax.scatter(*gather[self.affected_coords_cols].T, color=self.aux_color,
-                                                     marker=self.aux_marker, **self.scatter_kwargs)
+    def _plot_activated(self, coords):
+        if self.activated_scatter is not None:
+            self.activated_scatter.remove()
+        self.activated_scatter = self.main.ax.scatter(*coords.T, color=self.aux_color, marker=self.aux_marker,
+                                                      **self.scatter_kwargs)
+        self.main.fig.canvas.draw_idle()
 
+    def click(self, coords):
+        """Highlight activated shot or receiver locations and display a gather."""
+        coords = tuple(self.coords[self.coords_neighbors.kneighbors([coords], return_distance=False).item()])
+        gather_indices = self.active_map.get_indices_by_map_coords(coords).index.tolist()
+        gather_coords = [coords] * len(gather_indices)
+        titles = self.active_map.construct_items_titles(gather_coords, gather_indices)
+        self.aux.process_map_click(gather_coords, gather_indices, titles)
         self.aux.box.layout.visibility = "visible"
-        self.aux.set_title(self.gather_title + f"{x, y}")
-        self.aux.clear()
-        gather.plot(ax=self.aux.ax, **self.gather_plot_kwargs)
-        return x, y
+        return coords
 
     def unclick(self):
         """Remove highlighted shot or receiver locations and hide the gather plot."""
-        if self.affected_scatter is not None:
-            self.affected_scatter.remove()
-            self.affected_scatter = None
+        if self.activated_scatter is not None:
+            self.activated_scatter.remove()
+            self.activated_scatter = None
         self.aux.clear()
         self.aux.box.layout.visibility = "hidden"
