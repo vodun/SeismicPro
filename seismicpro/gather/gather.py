89e684a6faddcd9bc798188d3f42a5370e91f067
anton-br <ziliboba753@gmail.com>
2023-03-01 18:47:44 +0300

    add nan processing in scale_standard in gather

--- seismicpro/gather/gather.py
+++ seismicpro/gather/gather.py
@@ -493,8 +493,8 @@ class Gather(TraceContainer, SamplesContainer):
             mean = self.survey.mean
             std = self.survey.std
         else:
-            mean = self._apply_agg_func(func=np.mean, tracewise=tracewise, keepdims=True)
-            std = self._apply_agg_func(func=np.std, tracewise=tracewise, keepdims=True)
+            mean = self._apply_agg_func(func=np.nanmean, tracewise=tracewise, keepdims=True)
+            std = self._apply_agg_func(func=np.nanstd, tracewise=tracewise, keepdims=True)
         self.data = normalization.scale_standard(self.data, mean, std, np.float32(eps))
         return self
 
@@ -802,11 +802,11 @@ class Gather(TraceContainer, SamplesContainer):
         Calculate vertical velocity spectrum with default parameters: velocities evenly spaces around default stacking
         velocity, 50 ms temporal window size, semblance coherency measure and no muting of hodograph stretching:
         >>> velocity_spectrum = gather.calculate_vertical_velocity_spectrum()
-    
+
         Calculate vertical velocity spectrum for 200 velocities from 2000 to 6000 m/s, temporal window size of 128 ms,
         crosscorrelation coherency measure and muting stretching effects greater than 0.65:
         >>> velocity_spectrum = gather.calculate_vertical_velocity_spectrum(
-                                                                velocities=np.linspace(2000, 6000, 200), 
+                                                                velocities=np.linspace(2000, 6000, 200),
                                                                 window_size=128, mode='CC', max_stretch_factor=0.65)
 
         Parameters
@@ -820,8 +820,8 @@ class Gather(TraceContainer, SamplesContainer):
             Temporal window size used for velocity spectrum calculation. The higher the `window_size` is, the smoother
             the resulting velocity spectrum will be but to the detriment of small details. Measured in ms.
         mode: str, optional, defaults to 'semblance'
-            The measure for estimating hodograph coherency. 
-            The available options are: 
+            The measure for estimating hodograph coherency.
+            The available options are:
                 `semblance` or `NE`,
                 `stacked_amplitude` or `S`,
                 `normalized_stacked_amplitude` or `NS`,
@@ -830,7 +830,7 @@ class Gather(TraceContainer, SamplesContainer):
         max_stretch_factor : float, defaults to np.inf
             Max allowable factor for the muter that attenuates the effect of waveform stretching after nmo correction.
             This mute is applied after nmo correction for each provided velocity and before coherency calculation.
-            The lower the value, the stronger the mute. In case np.inf(default) no mute is applied. 
+            The lower the value, the stronger the mute. In case np.inf(default) no mute is applied.
             Reasonably good value is 0.65
 
         Returns
@@ -861,22 +861,22 @@ class Gather(TraceContainer, SamplesContainer):
         Parameters
         ----------
         stacking_velocity : StackingVelocity or StackingVelocityField or str
-            Stacking velocity around which residual velocity spectrum is calculated. 
-            `StackingVelocity` instance is used directly. If `StackingVelocityField` instance is passed, 
+            Stacking velocity around which residual velocity spectrum is calculated.
+            `StackingVelocity` instance is used directly. If `StackingVelocityField` instance is passed,
             a `StackingVelocity` corresponding to gather coordinates is fetched from it.
             May be `str` if called in a pipeline: in this case it defines a component with stacking velocities to use.
         n_velocities : int, optional, defaults to 140
             The number of velocities to compute residual velocity spectrum for.
         relative_margin : float, optional, defaults to 0.2
-            Relative velocity margin, that determines the velocity range for residual spectrum calculation 
+            Relative velocity margin, that determines the velocity range for residual spectrum calculation
             for each time `t` as `stacking_velocity(t)` * (1 +- `relative_margin`).
         window_size : int, optional, defaults to 50
             Temporal window size used for residual velocity spectrum calculation. Measured in ms.
             The higher the `window_size` is, the smoother the resulting spectrum will be but to the
             detriment of small details.
         mode: str, optional, defaults to 'semblance'
-            The measure for estimating hodograph coherency. 
-            The available options are: 
+            The measure for estimating hodograph coherency.
+            The available options are:
                 `semblance` or `NE`,
                 `stacked_amplitude` or `S`,
                 `normalized_stacked_amplitude` or `NS`,
@@ -885,7 +885,7 @@ class Gather(TraceContainer, SamplesContainer):
         max_stretch_factor : float, defaults to np.inf
             Max allowable factor for the muter that attenuates the effect of waveform stretching after nmo correction.
             This mute is applied after nmo correction for each provided velocity and before coherency calculation.
-            The lower the value, the stronger the mute. In case np.inf(default) no mute is applied. 
+            The lower the value, the stronger the mute. In case np.inf(default) no mute is applied.
             Reasonably good value is 0.65
 
         Returns
@@ -974,7 +974,7 @@ class Gather(TraceContainer, SamplesContainer):
             Whether to mute areas where the time reversal occurred after nmo corrections.
         max_stretch_factor : float, defaults to np.inf
             Max allowable factor for the muter that attenuates the effect of waveform stretching after nmo correction.
-            The lower the value, the stronger the mute. In case np.inf(default) no mute is applied. 
+            The lower the value, the stronger the mute. In case np.inf(default) no mute is applied.
             Reasonably good value is 0.65
         fill_value : float, optional, defaults to np.nan
             Value used to fill the amplitudes outside the gather bounds after moveout.
@@ -1059,9 +1059,9 @@ class Gather(TraceContainer, SamplesContainer):
         ----------
         amplify_factor : float in range [0, 1], optional, defaults to 0
             Amplifying factor which affects the normalization of the sum of hodographs amplitudes.
-            The amplitudes sum is multiplied by amplify_factor/sqrt(N) + (1 - amplify_factor)/N, 
+            The amplitudes sum is multiplied by amplify_factor/sqrt(N) + (1 - amplify_factor)/N,
             where N is the number of live(non muted) amplitudes. Acts as the coherency amplifier for long hodographs.
-            Note that in case amplify_factor=0 (default), sum of trace amplitudes is simply divided by N, 
+            Note that in case amplify_factor=0 (default), sum of trace amplitudes is simply divided by N,
             so that stack amplitude is the average of ensemble amplitudes. Must be in [0, 1] range.
 
         Returns
@@ -1596,7 +1596,16 @@ class Gather(TraceContainer, SamplesContainer):
         # Add a top subplot for given header if needed and set plot title
         top_ax = ax
         if top_header is not None:
-            top_ax = self._plot_top_subplot(ax=ax, divider=divider, header_values=self[top_header], y_ticker=y_ticker)
+            if isinstance(top_header, str):
+                header_values=self[top_header]
+            elif isinstance(top_header, np.ndarray) and top_header.shape == (self.n_traces, ):
+                header_values = top_header
+            else:
+                warnings.warn("`top_header` should be `str` or `np.ndarray`")
+                header_values = None
+
+            if header_values is not None:
+                top_ax = self._plot_top_subplot(ax=ax, divider=divider, header_values=header_values, y_ticker=y_ticker)
 
         # Set axis ticks.
         self._set_x_ticks(ax, tick_src=x_tick_src, ticker=x_ticker)
