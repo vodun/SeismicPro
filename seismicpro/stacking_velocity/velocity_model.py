54def7e8cc8eb87376c9f7dfa16f51f47951ae16
Alexander Kuvaev <alexandrkuvaev@yandex.ru>
2023-05-10 12:48:18 +0300

    Add velocity model docs

--- seismicpro/stacking_velocity/velocity_model.py
+++ seismicpro/stacking_velocity/velocity_model.py
@@ -1,3 +1,5 @@
+"""Implements an algorithm for stacking velocity computation by vertical velocity spectrum"""
+
 import math
 
 import numpy as np
@@ -7,6 +9,8 @@ from numba import njit, prange
 
 @njit(nogil=True)
 def get_path_sum(spectrum_data, start_time_ix, start_vel_ix, end_time_ix, end_vel_ix):
+    """Calculate sum of spectrum values along a line connecting 2 points with indices (`start_time_ix`, `start_vel_ix`)
+    and (`end_time_ix`, `end_vel_ix`). `start_vel_ix` and `end_vel_ix` may be floats."""
     n_points = end_time_ix - start_time_ix
     dv = (end_vel_ix - start_vel_ix) / n_points
     res = 0
@@ -24,6 +28,8 @@ def get_path_sum(spectrum_data, start_time_ix, start_vel_ix, end_time_ix, end_ve
 def create_edges_between_layers(spectrum_data, start_time, start_time_ix, start_velocities, start_velocities_ix,
                                 start_bias, end_time, end_time_ix, end_velocities, end_velocities_ix, end_bias,
                                 acceleration_bounds):
+    """Return edges connecting nodes with given `start_time` and `start_velocities` and nodes with given `end_time` and
+    `end_velocities` with their weights."""
     dt = (end_time - start_time) / 1000
     edges = []
     for start_vel_pos, (start_vel, start_vel_ix) in enumerate(zip(start_velocities, start_velocities_ix)):
@@ -39,6 +45,7 @@ def create_edges_between_layers(spectrum_data, start_time, start_time_ix, start_
 @njit(nogil=True, parallel=True)
 def create_edges(spectrum_data, node_times, node_times_ix, node_velocities, node_velocities_ix, node_biases,
                  max_n_skips, acceleration_bounds):
+    """Return edges of a graph for stacking velocity computation with their weights."""
     edges = [[(1, 1, -1.0)] for _ in range((max_n_skips + 1) * (len(node_times_ix) - 1))]
     for ix in prange(len(edges)):  # pylint: disable=not-an-iterable
         i = ix // (max_n_skips + 1)
@@ -59,6 +66,69 @@ def create_edges(spectrum_data, node_times, node_times_ix, node_velocities, node
 # pylint: disable-next=too-many-statements
 def calculate_stacking_velocity(spectrum, init=None, bounds=None, relative_margin=0.2, acceleration_bounds="auto",
                                 times_step=100, max_offset=5000, hodograph_correction_step=25, max_n_skips=2):
+    """Calculate stacking velocity by vertical velocity spectrum.
+
+    Stacking velocity is the value of the seismic velocity obtained from the best fit of the traveltime curve by a
+    hyperbola for each timestamp. It is used to correct the arrival times of reflection events in the traces for their
+    varying offsets prior to stacking.
+
+    If calculated by velocity spectrum, stacking velocity must meet the following conditions:
+    1. It should be monotonically increasing,
+    2. Its gradient should be bounded above to avoid excessive gather stretching after NMO correction,
+    3. It should pass through local energy maxima on the velocity spectrum.
+
+    In order for these conditions to be satisfied, the following algorithm is proposed:
+    1. Stacking velocity is being found inside an area bounded both left and right by two stacking velocities defined
+       by `bounds`. If `bounds` are not given, they are assumed to be [`init` * (1 - `relative_margin`),
+       `init` * (1 + `relative_margin`)], where `init` is a rough estimate of the stacking velocity being picked.
+    2. An auxiliary directed acyclic graph is constructed so that:
+        1. The whole time range of the velocity spectrum is covered with a grid of times with a step of `times_step`.
+        2. A range of velocities is selected for each of these times so that:
+            1. They cover the whole range of allowed velocities according to selected `bounds`,
+            2. A difference between two adjacent velocities is so that two hodographs starting with these velocities
+               from their common zero-offset time arrive at `max_offset` within `hodograph_correction_step`
+               milliseconds from each other.
+        3. Each (time, velocity) pair defines a node of the graph. An edge between nodes A and B exists only if:
+            1. Time at node B is greater than that of node A but by no more than (`max_n_skips` + 1) * `times_step`,
+            2. Transition from node A to B occurs with acceleration within provided `acceleration_bounds`.
+        4. Edge weight is defined as a sum of velocity spectrum values along its path.
+    3. A path with maximal velocity spectrum sum along it between any of starting and ending nodes is found using
+       Dijkstra algorithm and is considered to be the required stacking velocity.
+
+    Parameters
+    ----------
+    spectrum : VerticalVelocitySpectrum
+        Vertical velocity spectrum to calculate stacking velocity for.
+    init : StackingVelocity, optional
+        A rough estimate of the stacking velocity being picked. Used to calculate `bounds` as
+        [`init` * (1 - `relative_margin`), `init` * (1 + `relative_margin`)] if they are not given.
+    bounds : array-like of two StackingVelocity, optional
+        Left and right bounds of an area for stacking velocity picking. If not given, `init` must be passed.
+    relative_margin : positive float, optional, defaults to 0.2
+        A fraction of stacking velocities defined by `init` used to estimate `bounds` if they are not given.
+    acceleration_bounds : tuple of two positive floats or "auto" or None, optional
+        Minimal and maximal acceleration allowed for the stacking velocity function. If "auto", equals to the range of
+        accelerations of stacking velocities in `bounds` extended by 50% in both directions. If `None`, only ensures
+        that picked stacking velocity is monotonically increasing. Measured in meters/seconds^2.
+    times_step : float, optional, defaults to 100
+        A difference between two adjacent times defining graph nodes.
+    max_offset : float, optional, defaults to 5000
+        An offset for hodograph time estimation. Used to create graph nodes and calculate their velocities for each
+        time.
+    hodograph_correction_step : float, optional, defaults to 25
+        The maximum difference in arrival time of two hodographs starting at the same zero-offset time and two adjacent
+        velocities at `max_offset`. Used to create graph nodes and calculate their velocities for each time.
+    max_n_skips : int, optional, defaults to 2
+        Defines the maximum number of subsequent times to skip to still be able to connect two nodes of the graph with
+        an edge.
+
+    Returns
+    -------
+    times : 1d np.ndarray
+        Times for which stacking velocities were picked. Measured in milliseconds.
+    velocities : 1d np.ndarray
+        Picked stacking velocities. Matches the length of `times`. Measured in meters/seconds.
+    """
     spectrum_data = spectrum.velocity_spectrum.max() - spectrum.velocity_spectrum
     spectrum_times = np.asarray(spectrum.times, dtype=np.float32)
     spectrum_velocities = np.asarray(spectrum.velocities, dtype=np.float32)
@@ -117,17 +187,20 @@ def calculate_stacking_velocity(spectrum, init=None, bounds=None, relative_margi
 
     # Create a graph and find the path with maximal velocity spectrum sum along it
     n_nodes = 2 + sum(len(node_vels) for node_vels in node_velocities)
+    start_node = 0
+    end_node = n_nodes - 1
     node_biases = np.cumsum([1] + [len(node_vels) for node_vels in node_velocities[:-1]])
     edges = create_edges(spectrum_data, node_times, node_times_ix, node_velocities, node_velocities_ix, node_biases,
                          max_n_skips, acceleration_bounds)
     graph = rx.PyDiGraph()
     for layer_edges in edges:
         graph.extend_from_weighted_edge_list(layer_edges)
-    end_node = n_nodes - 1
-    paths_dict = rx.dijkstra_shortest_paths(graph, 0, end_node, weight_fn=float)
+    paths_dict = rx.dijkstra_shortest_paths(graph, start_node, end_node, weight_fn=float)
     if end_node not in paths_dict:
         raise ValueError("No path was found for given parameters")
     path = np.array(paths_dict[end_node], dtype=np.int32)[1:-1]
+
+    # Convert the path to arrays of times and stacking velocities
     times_ix = np.searchsorted(node_biases, path, side="right") - 1
     vels_ix = path - node_biases[times_ix]
     times = node_times[times_ix]
