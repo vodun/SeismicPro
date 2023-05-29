fix len in normalization

seismicpro/gather/gather.py
@@ -550,7 +550,7 @@ class Gather(TraceContainer, SamplesContainer):
         elif not tracewise:
             min_value, max_value = self.get_quantile([q_min, q_max], tracewise=False)
         else:
-            min_value, max_value = normalization.get_quantile(self.data, np.array([q_min, q_max]))
+            min_value, max_value = normalization.get_quantile(self.data,[q_min, q_max])
         # Use np.atleast_2d(array).T to make the array 2-dimensional by adding dummy trailing axes
         # for further broadcasting to work tracewise
         min_value, max_value = np.atleast_2d(min_value), np.atleast_2d(max_value)

seismicpro/gather/utils/normalization.py
@@ -93,7 +93,7 @@ def get_quantile(data, q):
     q : 2d np.ndarray of floats
         The array with `q`-th quantile values.
     """
-    n_traces, n_quantiles = len(data), q.size
+    n_traces, n_quantiles = len(data), len(q)
     values = np.empty((n_quantiles, n_traces), dtype=np.float64)
     for i in prange(n_traces):
         values[:, i] = np.nanquantile(data[i], q=q)

seismicpro/refractor_velocity/metrics.py
@@ -6,7 +6,6 @@ import numpy as np
 from numba import njit
 from scipy.signal import hilbert
 
-from ..gather import Gather
 from ..gather.utils.normalization import scale_maxabs, get_quantile
 from ..gather.utils.correction import get_hodograph
 from ..metrics import Metric
@@ -418,10 +417,10 @@ class FirstBreaksCorrelations(RefractorVelocityMetric):
                 plotting_kwargs["masks"] = {"masks": metric_values * -1, "threshold": mask_threshold * -1}
         traces_windows = self._make_windows(gather[self.first_breaks_header], gather.data, gather["offset"],
                                             self.window_size, gather.sample_interval, gather.delay)
-        window_gather = Gather(headers=gather.headers, data=traces_windows, sample_interval=gather.sample_interval,
-                               survey=gather.survey, delay=0)
+        gather.data = traces_windows
+        gather.samples = gather.create_samples(traces_windows.shape[1], gather.sample_interval, gather.delay)
         plotting_kwargs.update(kwargs)
-        window_gather.plot(ax=ax, **plotting_kwargs)
+        gather.plot(ax=ax, **plotting_kwargs)
         set_ticks(ax, axis="y", label="Time from first break, ms",
                   num=min(self.window_size // int(gather.sample_interval), 9),
                   major_labels=np.arange(-self.window_size // 2, self.window_size // 2 + 1, int(gather.sample_interval),
@@ -435,10 +434,10 @@ class FirstBreaksCorrelations(RefractorVelocityMetric):
                                             self.window_size, gather.sample_interval, gather.delay)
         mean_hodograph = np.nanmean(traces_windows, axis=0)
         mean_hodograph_scaled = ((mean_hodograph - mean_hodograph.mean()) / mean_hodograph.std()).reshape(1, -1)
-        fb_gather = Gather(headers=gather.headers, data=mean_hodograph_scaled, sample_interval=gather.sample_interval,
-                           survey=gather.survey, delay=0)
 
-        fb_gather.plot(mode="wiggle", ax=ax, **kwargs)
+        gather.data = mean_hodograph_scaled
+        gather.samples = gather.create_samples(mean_hodograph_scaled.shape[1], gather.sample_interval, gather.delay)
+        gather.plot(mode="wiggle", ax=ax, **kwargs)
         fb_time_mean = np.mean(gather[self.first_breaks_header], dtype=np.int32)
         set_ticks(ax, axis="x", label="Amplitude", num=3)
         set_ticks(ax, axis="y", label="Time, ms",