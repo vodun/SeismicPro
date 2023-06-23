simplify avo qc, fix avo computation with horizon

seismicpro/avo/avo.py
@@ -16,18 +16,19 @@ class AmplitudeOffsetDistribution:
         self.name = name
 
         if isinstance(bin_size, (int, np.integer)):
-            # add +1 to avoid the case of falling outside the `bin_bounds` if headers offset equal to bin_bounds[-1]
-            bin_bounds = np.arange(0, self.headers["offset"].max()+bin_size+1, bin_size)
+            bin_bounds = np.arange(0, self.headers["offset"].max()+bin_size, bin_size)
         else:
             bin_bounds = np.cumsum([0, *bin_size])
 
-        self.headers["bin"] = bin_bounds[np.searchsorted(bin_bounds, self.headers["offset"], side='right')]
+        # Subtract 1 to start at offset 0 instead of `bin_size`.
+        bin_bounds_ixs = np.searchsorted(bin_bounds, self.headers["offset"], side='right') - 1
+        self.headers["bin"] = bin_bounds[np.clip(bin_bounds_ixs, 0, len(bin_bounds))]
         self.stats_df = self.headers.groupby([*to_list(indexed_by), "bin"], as_index=False)[avo_column].mean()
         self.bins_df = self.stats_df.groupby("bin", as_index=False)[avo_column].mean()
 
         # Metrics
         self.metrics = {}
-        self.qc(names=["std", "corr"], pol_degree=pol_degree)
+        self.qc(pol_degree=pol_degree)
 
     @classmethod
     def from_survey(cls, survey, avo_column, bin_size, indexed_by=None, name=None, pol_degree=3):
@@ -36,24 +37,14 @@ class AmplitudeOffsetDistribution:
         return cls(headers=survey.headers, avo_column=avo_column, bin_size=bin_size, indexed_by=indexed_by, name=name,
                    pol_degree=pol_degree)
 
-    def qc(self, names=None, **kwargs):
-        metrics_dict = {
-            "std": self._calculate_std,
-            "corr": self._calculate_corr
-        }
-        names = names if names is not None else ["std", "corr"]
-        for name in to_list(names):
-            metric_func = metrics_dict.get(name)
-            if metric_func is None:
-                raise ValueError("")
-            self.metrics[name] = metric_func(**kwargs)
+    def qc(self, pol_degree=3):
+        self.metrics["std"] = self._calculate_std()
+        self.metrics["corr"] = self._calculate_corr(pol_degree=pol_degree)
 
-    def _calculate_std(self, **kwargs):
-        _ = kwargs
+    def _calculate_std(self):
         return self.stats_df.groupby("bin")[self.avo_column].apply(np.nanstd).mean()
 
-    def _calculate_corr(self, pol_degree=3, **kwargs):
-        _ = kwargs
+    def _calculate_corr(self, pol_degree):
         mask = ~self.bins_df[self.avo_column].isna()
         not_nan_bins = self.bins_df[mask]
         poly = np.polyfit(not_nan_bins["bin"], not_nan_bins[self.avo_column], deg=pol_degree)

seismicpro/gather/gather.py
@@ -315,7 +315,7 @@ class Gather(TraceContainer, SamplesContainer):
                 headers[column] = headers[column].astype(column_data.dtype)
 
             # Avoid warning related to the workaround
-            with warnings.catch_warnings(SettingWithCopyWarning):
+            with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
                 # FIXME: Workaround for a pandas bug https://github.com/pandas-dev/pandas/issues/48998
                 # iloc may call unnecessary copy of the whole column before setitem
@@ -1317,10 +1317,9 @@ class Gather(TraceContainer, SamplesContainer):
             if len(window) != 2:
                 raise ValueError(f"`window` must have exact two elements, not {len(window)}")
             limits = self.times_to_indices(np.array(window), round=True).astype(np.int32)
-            # Include the next index to mimic the behavior of conventional software
-            limits[1] += 1
             start_ixs = np.full(self.n_traces, fill_value=limits[0], dtype=np.int32)
-            end_ixs = np.full(self.n_traces, fill_value=limits[1], dtype=np.int32)
+            # Include the next index to mimic the behavior of conventional software
+            end_ixs = np.full(self.n_traces, fill_value=limits[1] + 1, dtype=np.int32)
 
         elif horizon_header is not None:
             centers = self[horizon_header]
@@ -1335,7 +1334,8 @@ class Gather(TraceContainer, SamplesContainer):
             start_times = np.clip(centers - horizon_window_size[0], 0, self.samples[-1])
             start_ixs = self.times_to_indices(start_times, round=True).astype(np.int32)
             end_times = np.clip(centers + horizon_window_size[1], 0, self.samples[-1])
-            end_ixs = self.times_to_indices(end_times, round=True).astype(np.int32)
+             # Include the next index to mimic the behavior of conventional software
+            end_ixs = self.times_to_indices(end_times, round=True).astype(np.int32) + 1
 
         func_dict = {
             "rms": stats.numba_rms,
@@ -1346,7 +1346,9 @@ class Gather(TraceContainer, SamplesContainer):
         if method is None:
             raise ValueError(f"`mode` should be either `abs` or `rms`, not {mode}")
         data = np.where(self.data != 0, self.data, np.nan)
-        self[avo_col] = method(data, start_ixs, end_ixs)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self[avo_col] = method(data, start_ixs, end_ixs)
         return self
 
     #------------------------------------------------------------------------#